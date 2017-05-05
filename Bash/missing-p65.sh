fbd3fbe967080e9ecf298d0dd9fc144e70a455ec
erik-morgan <morgan.erik@outlook.com>
2017-05-04 20:52:16 -0500

    Added script for missing library files

---
+++ Bash/missing-p65.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash
+while IFS= read -r N
+do
+	FILES=$(find ~/ -type f -not -path "*/PM2INDD/*" -name "$N*" -not -name "*.???" -print)
+	for F in $FILES
+	do
+		F_KIND=$(mdls -name 'kMDItemKind' "$F")
+		if [[ "$F_KIND" =~ .*InDesign.* ]] || [[ "$F_KIND" =~ .*PageMaker.* ]]
+		then
+			cp -a "$F" /Users/HD6904/Erik/PM2INDD/MISSING
+		fi
+	done
+done < /Users/HD6904/Erik/PM2INDD/MISSING.txt
