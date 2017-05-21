0f9103d67d429b7bea435c15bc348631a96f3858
erik-morgan <morgan.erik@outlook.com>
2017-05-21 14:57:17 -0500

    Update ShowHide Local Formatting.jsx

--- JavaScript/InDesign/ShowHide Local Formatting.jsx
+++ JavaScript/InDesign/ShowHide Local Formatting.jsx
@@ -1,1 +1,28 @@
-#target indesignvar doc = app.activeDocument;/*app.findGrepPreferences = app.changeGrepPreferences = app.findChangeGrepOptions = app.findTextPreferences = app.changeTextPreferences = app.findChangeTextOptions = NothingEnum.nothing;app.findGrepPreferences.findWhat = '(?i)suggested procedure';var mainStory = doc.findGrep()[0].parentStory;var mainChars = mainStory.characters.everyItem().getElements();if (doc.label !== 'ENABLED'){	doc.label = 'ENABLED';	enableHighlights();}else {	disableHighlights();}function enableHighlights(){	for (i = 0; i < mainChars.length; i++){		mainChars[i].*/if (doc.textPreferences.enableStylePreviewMode == true){	doc.textPreferences.enableStylePreviewMode = false;}else {	doc.textPreferences.enableStylePreviewMode = true;}
+#target indesign
+var doc = app.activeDocument;
+/*
+app.findGrepPreferences = app.changeGrepPreferences = app.findChangeGrepOptions = app.findTextPreferences = app.changeTextPreferences = app.findChangeTextOptions = NothingEnum.nothing;
+app.findGrepPreferences.findWhat = '(?i)suggested procedure';
+var mainStory = doc.findGrep()[0].parentStory;
+
+var mainChars = mainStory.characters.everyItem().getElements();
+
+if (doc.label !== 'ENABLED'){
+	doc.label = 'ENABLED';
+	enableHighlights();
+}
+else {
+	disableHighlights();
+}
+
+function enableHighlights(){
+	for (i = 0; i < mainChars.length; i++){
+		mainChars[i].
+*/
+
+if (doc.textPreferences.enableStylePreviewMode == true){
+	doc.textPreferences.enableStylePreviewMode = false;
+}
+else {
+	doc.textPreferences.enableStylePreviewMode = true;
+}
