3faab998e8ad151801b667027fe3f684f9d8faf7
Erik Morgan <morgan.erik@outlook.com>
2017-06-24 09:46:59 -0500

    add drawing revision updating scripts

---
+++ Bash/replaceDrawings.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+update () {
+	pn="$1"
+	oldrev="$2"
+	newrev="$3"
+	hopper="$4"
+	dwgs=$(dirname "$hopper")
+	dirpath=$(find "$dwgs" -path '*/~*' -name "${pn}*pdf" -exec mv "{}" "$hopper"; dirname "{}" \; | head -n 1)
+	cd "$hopper"
+	filenames=$(find . -not -name "${pn}.pdf" -name "${pn}*pdf" -exec basename "{}" \;)
+	pgnum=$(pdftk "${pn}.pdf" dump_data | grep "NumberOfPages" | cut -d" " -f2)
+	for file in $filenames
+	do
+		pdfname="${file/.$oldrev./.$newrev.}"
+		pdftk A="${pn}.pdf" B="$file" cat A B$((pgnum+1))-Bend output "${dirpath}/$pdfname"
+	done
+	rm "${pn}*pdf"
+}
+
+replace () {
+	pn="$1"
+	newrev="$2"
+	hopper="$3"
+	dwgs=$(dirname "$hopper")
+	dirpath=$(find "$dwgs" -path '*/~*' -name "${pn}*pdf" -exec dirname "{}" \; -delete)
+	cd "$hopper"
+	mv "${pn}.pdf" "$dirpath/${pn}.${newrev}.pdf"
+	rm "${pn}*pdf"
+}
+
+if [[ "$#" -eq 3 ]]
+then
+	replace "$1" "$2" "$3"
+else
+	update "$1" "$2" "$3" "$4"
+fi
+echo 'true'
---
+++ Bash/replaceSpecSheets.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+pn="$1"
+pl="$2"
+hopper="$3"
+pdfname="$4"
+dwgs=$(dirname "$hopper")
+
+curl -o "$hopper${pl}.pdf" "http://houston/ErpWeb/Part/PartDocumentReader.aspx?partnumber=${pl}&checkInProcess=1" -H 'Cookie: DqUserInfo=PartDocumentReader=AMERICAS\MorganEL' -H 'Connection: keep-alive' --compressed
+
+while [[ ! -e "$hopper${pl}.pdf" ]]
+do
+	sleep 1
+done
+
+dirpath=$(find "$dwgs" -path '*/~*' -name "${pl}*pdf" -exec mv "{}" "$hopper${pn}.pdf" \; -exec dirname "{}" \;)
+cd "$hopper"
+pgnum=$(pdftk "${pl}.pdf" dump_data | grep "NumberOfPages" | cut -d" " -f2)
+pdftk A="${pn}.pdf" B="${pl}.pdf" cat Arend-r$((pgnum+1)) B output "${dirpath}/$pdfname"
+rm *.pdf
+echo 'true'
