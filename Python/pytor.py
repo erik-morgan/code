6994c0062a99cacdaf6873b114f165c3576a9608
Erik Morgan <morgan.erik@outlook.com>
2017-11-26 00:07:46 -0600

    added multiprocessing

--- Python/pytor.py
+++ Python/pytor.py
@@ -1,56 +1,57 @@
 from lxml import html, etree
-from math import floor, pow, log
-from multiprocessing import Pool
-import requests, time, json
+from multiprocessing.dummy import Pool as ThreadPool
+from requests import get as req
+import time, json, logging
 
-# create a main function and use low(q)/low(cat)
-q = 'the+librarians'
-cat = 'tv'
-low = str.lower
+logging.basicConfig(
+    # this logging stuff MIGHT go into a different file
+    filename='pytor.log',
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
 
-def search ():
-    
-    search_results = []
-    ext = search_results.extend
-    htmlstr = html.fromstring
-    req = requests.get
+def search (q, cat):
+
+    logger.info('beginning search')
+    torlist = []
+    ext = torlist.extend
     jtkn = req('https://torrentapi.org/pubapi_v2.php?get_token=get_token')
     token = json.loads(jtkn.text)['token']
-    
+
     def eztv ():
         results = []
+        htmlstr = html.fromstring
         url = f'https://eztv.ag/search/{q}'
-        tree = htmlstr(req(url).content)
+        tree = htmlstr(req(url, timeout=3).content)
         x = tree.xpath
         add = results.append
-        rstop = str.rindex
         items = list(zip(
             x('//tr[@name="hover"]//a[@class="epinfo"]/@title'),
             x('//tr[@name="hover"]/td[4]/text()'),
             x('//tr[@name="hover"]/td[6]//text()'),
             x('//tr[@name="hover"]/td[3]/a[1]/@href'),
+            x('//tr[@name="hover"]/td[1]/a/@title')
         ))
-        shows = x('//tr[@name="hover"]/td[1]/a/@title')
-        for i, item in enumerate(items):
-            link, seed = item[3], item[2]
-            show = shows[i]
+        for item in items:
+            seed, link, show = item[2:]
             if 'Other' not in show and seed != '-' and int(seed) > 0 and link[0:6] == 'magnet':
                 add([
-                    item[0][:rstop(item[0], ' (')],
+                    item[0][:-(len(item[1]) + 3)],
                     item[1],
                     seed,
                     '-',
                     link,
                     link[20:60]
                 ])
+        print('eztv processed successfully...')
         return results
-    
+
     def lime ():
-        results = []
+        htmlstr = html.fromstring
         url = f'https://www.limetorrents.cc/search/{cat}/{q}/seeds/1/'
-        tree = htmlstr(req(url).content)
+        tree = htmlstr(req(url, timeout=3).content)
         x = tree.xpath
-        add = results.append
         items = list(zip(
             x('//div[@class="tt-name"]//a[2]/text()'),
             x('//td[@class="tdnormal"][2]/text()'),
@@ -58,13 +59,11 @@ def search ():
             x('//table[2]//td[@class="tdleech"]/text()'),
             x('//a[@class="csprite_dl14"]/@href')
         ))
-        [add(list(item) + [item[4][29:69]]) for item in items if int(item[2]) > 0]
-        return results
-    
+        print('lime processed successfully...')
+        return [list(item) + [item[4][29:69]] for item in items if int(item[2]) > 0]
+
     def rarbg ():
-        results = []
         url = 'https://torrentapi.org/pubapi_v2.php'
-        add = results.append
         payload = {
             'mode': 'search',
             'search_string': q,
@@ -75,22 +74,16 @@ def search ():
             'format': 'json_extended',
             'token': token
         }
-        data = json.loads(req(url, params=payload).text)
-        for item in data['torrent_results']:
-            add([
-                item['title'],
-                item['size'],
-                item['seeders'],
-                item['leechers'],
-                item['download'],
-                item['download'][20:60]
-            ])
-        return results
-    
+        items = json.loads(req(url, timeout=3, params=payload).text)['torrent_results']
+        print('rarbg processed successfully...')
+        keys = ['title', 'size', 'seeders', 'leechers', 'download']
+        return [[item[key] for key in keys] + [item['download'][20:60]] for item in items]
+
     def tpb ():
         results = []
+        htmlstr = html.fromstring
         url = f'https://thepiratebay.org/search/{q}/0/7/200/'
-        tree = htmlstr(req(url).content)
+        tree = htmlstr(req(url, timeout=3).content)
         x = tree.xpath
         add = results.append
         sep = str.split
@@ -100,11 +93,11 @@ def search ():
             x('//td[@align="right"][1]/text()'),
             x('//td[@align="right"][2]/text()'),
             x('//a[contains(@href, "magnet")]/@href'),
+            x('//td[@class="vertTh"]//a[2]/text()')
         ))
-        tcats = x('//td[@class="vertTh"]//a[2]/text()')
-        for i, item in enumerate(items):
-            tcat = tcats[i]
-            if cat in low(tcat) and int(item[2]) > 0:
+        low = str.lower
+        for item in items:
+            if cat in low(item[5]) and int(item[2]) > 0:
                 add([
                     item[0],
                     sep(item[1], ', ')[1][5:-2] + 'B',
@@ -113,11 +106,13 @@ def search ():
                     item[4],
                     item[4][20:60]
                 ])
-    
+        print('tpb processed successfully...')
+        return results
+
     def zoo ():
         xmlstr = etree.fromstring
         url = f'https://zooqle.com/search?q={q}+category%3A{cat}&sd=d&fmt=rss&pg='
-        pages = [req(url + str(p)).content for p in [1, 2, 3]]
+        pages = [req(url + str(p), timeout=3).content for p in [1, 2, 3]]
         trees = [xmlstr(page)[0] for page in pages]
         items = list(zip(
             [item[0].text for tree in trees for item in tree[8:]],
@@ -127,50 +122,64 @@ def search ():
             [item[8].text for tree in trees for item in tree[8:]],
             [item[7].text for tree in trees for item in tree[8:]]
         ))
+        print('zoo processed successfully...')
         return [list(item) for item in items]
-    
+
+    def filtor (tors):
+        # if ascii(s) !== s: proceed || all(ord(char) < 128 for char in tname)
+        flags = ['rus', 'fuck', 'anal', 'xxx']
+        sep = str.split
+        qs = sep(q, '+')
+        low = str.lower
+        seen = {}
+        for tor in tors:
+            tname, tseed, thash = low(tor[0]), tor[2], tor[5]
+            try:
+                tname.encode('ascii')
+                words = sep(tname.replace('.', ' '), ' ')
+                if thash not in seen or int(tseed) > int(seen[thash][2]):
+                    if all(flag not in words for flag in flags) and all(term in tname for term in qs):
+                        seen[thash] = tor
+            except UnicodeEncodeError:
+                continue
+        print('filtor processed successfully...')
+        return seen.values()
+
+    def run_func (f):
+        try:
+            ext(f())
+        except Exception as err:
+            logging.debug(err, exc_info=True)
+
     funcs = [eztv, lime, rarbg, tpb, zoo]
-    for func in funcs:
-        try:
-            ext(func())
-        except Exception as e:
-            # log error to logfile with traceback
-            ename = type(e).__name__
-            print(ename)
-            continue
-    
-    return search_results
+    if cat != 'tv':
+        funcs.pop(0)
+    pool = ThreadPool(5)
+    pool.map_async(run_func, funcs)
+    pool.close()
+    pool.join()
+    logger.info('search complete, attempting to run filtor...')
+    try:
+        torlist = filtor(torlist)
+        print('execution complete!\n# of results = ' + str(len(torlist)))
+        return torlist
+    except Exception as e:
+        print('problem during filtor...')
+        logging.exception(e)
 
-def filtor(tors):
-    # test whether its faster to do nametest during hash filtering, even if tor gets replaced
-    # if ascii(s) !== s: proceed
-    # all(ord(char) < 128 for char in tname)
-    flags = ['rus', 'fuck', 'anal', 'xxx']
-    sep = str.split
-    qs = sep(q, '+')
-    seen = {}
-    for tor in tors:
-        tname, tseed, thash = low(tor[0]), tor[2], tor[5]
-        try:
-            tname.encode('ascii')
-            words = sep(tname.replace('.', ' '), ' ')
-            if thash not in seen or int(tseed) > int(seen[thash][2]):
-                if all(flag not in words for flag in flags) and all(term in tname for term in qs):
-                    seen[thash] = tor
-        except UnicodeEncodeError:
-            continue
-    return seen.values()
-
-torrents = filtor(search())
+t = time.time()
+torrents = search('the+librarians', 'tv')
+print(time.time() - t)
 
 # Torrent = [name, size, seed, peer, link, hash]
 #
-# Implement multiprocessing/multithreading
-# Wrap each function in try/except so if one fails, the script will succeed
-#
 # sort by seeders
 # do byte conversions when putting in html
+# from math import floor, pow, log
 # if type(tor[1]) is int:
 #     i = int(floor(log(int(b), 1024)))
 #     str(round(int(b) / pow(1024, i), 2)) + ' ' + ('KB', 'MB', 'GB')[i]
-# use if __name__ == '__main__': main()
+# torlock has a good selection, cats, sorting, and 75 per page all 0 seeds
+# leetx no magnets
+# magnetdl has sorting, cats, magnets, 40 per page, but results are iffy correct seeds
+# http://www.magnetdl.com/t/the-librarians/se/desc/
---
+++ Python/pytor_multiprocessing.py
@@ -0,0 +1,193 @@
+from lxml import html, etree
+from multiprocessing import Pool
+from requests import get as req
+import time, json, logging
+
+logging.basicConfig(
+    # this logging stuff MIGHT go into a different file
+    filename='pytor.log',
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
+
+def eztv (q):
+    results = []
+    htmlstr = html.fromstring
+    url = f'https://eztv.ag/search/{q}'
+    tree = htmlstr(req(url, timeout=3).content)
+    x = tree.xpath
+    add = results.append
+    items = list(zip(
+        x('//tr[@name="hover"]//a[@class="epinfo"]/@title'),
+        x('//tr[@name="hover"]/td[4]/text()'),
+        x('//tr[@name="hover"]/td[6]//text()'),
+        x('//tr[@name="hover"]/td[3]/a[1]/@href'),
+        x('//tr[@name="hover"]/td[1]/a/@title')
+    ))
+    for item in items:
+        seed, link, show = item[2:]
+        if 'Other' not in show and seed != '-' and int(seed) > 0 and link[0:6] == 'magnet':
+            add([
+                item[0][:-(len(item[1]) + 3)],
+                item[1],
+                seed,
+                '-',
+                link,
+                link[20:60]
+            ])
+    print('eztv processed successfully...')
+    return results
+
+def lime (q, cat):
+    htmlstr = html.fromstring
+    url = f'https://www.limetorrents.cc/search/{cat}/{q}/seeds/1/'
+    tree = htmlstr(req(url, timeout=3).content)
+    x = tree.xpath
+    items = list(zip(
+        x('//div[@class="tt-name"]//a[2]/text()'),
+        x('//td[@class="tdnormal"][2]/text()'),
+        x('//table[2]//td[@class="tdseed"]/text()'),
+        x('//table[2]//td[@class="tdleech"]/text()'),
+        x('//a[@class="csprite_dl14"]/@href')
+    ))
+    print('lime processed successfully...')
+    return [list(item) + [item[4][29:69]] for item in items if int(item[2]) > 0]
+
+def rarbg (q, cat, token):
+    url = 'https://torrentapi.org/pubapi_v2.php'
+    payload = {
+        'mode': 'search',
+        'search_string': q,
+        'category': cat,
+        'limit': '100',
+        'sort': 'seeders',
+        'min_seeders': '1',
+        'format': 'json_extended',
+        'token': token
+    }
+    items = json.loads(req(url, timeout=3, params=payload).text)['torrent_results']
+    print('rarbg processed successfully...')
+    keys = ['title', 'size', 'seeders', 'leechers', 'download']
+    return [[item[key] for key in keys] + [item['download'][20:60]] for item in items]
+
+def tpb (q, cat):
+    results = []
+    htmlstr = html.fromstring
+    url = f'https://thepiratebay.org/search/{q}/0/7/200/'
+    tree = htmlstr(req(url, timeout=3).content)
+    x = tree.xpath
+    add = results.append
+    sep = str.split
+    items = list(zip(
+        x('//a[@class="detLink"]/text()'),
+        x('//font[@class="detDesc"]/text()'),
+        x('//td[@align="right"][1]/text()'),
+        x('//td[@align="right"][2]/text()'),
+        x('//a[contains(@href, "magnet")]/@href'),
+        x('//td[@class="vertTh"]//a[2]/text()')
+    ))
+    low = str.lower
+    for item in items:
+        if cat in low(item[5]) and int(item[2]) > 0:
+            add([
+                item[0],
+                sep(item[1], ', ')[1][5:-2] + 'B',
+                item[2],
+                item[3],
+                item[4],
+                item[4][20:60]
+            ])
+    print('tpb processed successfully...')
+    return results
+
+def zoo (q, cat):
+    xmlstr = etree.fromstring
+    url = f'https://zooqle.com/search?q={q}+category%3A{cat}&sd=d&fmt=rss&pg='
+    pages = [req(url + str(p), timeout=3).content for p in [1, 2, 3]]
+    trees = [xmlstr(page)[0] for page in pages]
+    items = list(zip(
+        [item[0].text for tree in trees for item in tree[8:]],
+        [int(item[6].text) for tree in trees for item in tree[8:]],
+        [item[9].text for tree in trees for item in tree[8:]],
+        [item[10].text for tree in trees for item in tree[8:]],
+        [item[8].text for tree in trees for item in tree[8:]],
+        [item[7].text for tree in trees for item in tree[8:]]
+    ))
+    print('zoo processed successfully...')
+    return [list(item) for item in items]
+
+def filtor (tors, q):
+    # if ascii(s) !== s: proceed || all(ord(char) < 128 for char in tname)
+    flags = ['rus', 'fuck', 'anal', 'xxx']
+    sep = str.split
+    qs = sep(q, '+')
+    low = str.lower
+    seen = {}
+    for tor in tors:
+        tname, tseed, thash = low(tor[0]), tor[2], tor[5]
+        try:
+            tname.encode('ascii')
+            words = sep(tname.replace('.', ' '), ' ')
+            if thash not in seen or int(tseed) > int(seen[thash][2]):
+                if all(flag not in words for flag in flags) and all(term in tname for term in qs):
+                    seen[thash] = tor
+        except UnicodeEncodeError:
+            continue
+    print('filtor processed successfully...')
+    return seen.values()
+
+def run_func (f):
+    func, args = f
+    try:
+        if type(args) != tuple:
+            return func(args)
+        return func(*args)
+    except Exception as err:
+        logging.debug(err, exc_info=True)
+        return []
+
+def search (q, cat):
+    logger.info('beginning search')
+    torlist = []
+    ext = torlist.extend
+    jtkn = req('https://torrentapi.org/pubapi_v2.php?get_token=get_token')
+    token = json.loads(jtkn.text)['token']
+
+    funcs = [
+        (eztv, q),
+        (lime, (q, cat)),
+        (rarbg, (q, cat, token)),
+        (tpb, (q, cat)),
+        (zoo, (q, cat))
+    ]
+    pool = Pool(5)
+    tormaps = pool.map(run_func, funcs)
+    pool.close()
+    pool.join()
+    [ext(tormap) for tormap in tormaps]
+    print('# of results (pre-filtor) = ' + str(len(torlist)))
+    logger.info('search complete, attempting to run filtor...')
+    try:
+        results = filtor(torlist, q)
+        print('# of results (post-filtor) = ' + str(len(results)))
+        return results
+    except Exception as e:
+        logging.exception(e)
+
+t = time.time()
+torrents = search('the+librarians', 'tv')
+print(time.time() - t)
+
+# Torrent = [name, size, seed, peer, link, hash]
+#
+# sort by seeders
+# do byte conversions when putting in html
+# from math import floor, pow, log
+# if type(tor[1]) is int:
+#     i = int(floor(log(int(b), 1024)))
+#     str(round(int(b) / pow(1024, i), 2)) + ' ' + ('KB', 'MB', 'GB')[i]
+# torlock has a good selection, cats, sorting, and 75 per page all 0 seeds
+# leetx no magnets
+# magnetdl has sorting, cats, magnets, 40 per page, but results are iffy correct seeds
+# http://www.magnetdl.com/t/the-librarians/se/desc/
