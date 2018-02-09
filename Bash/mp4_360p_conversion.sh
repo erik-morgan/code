f00cb64361b67ee4787e9ece0edc524d84d727b2
Erik Morgan <morgan.erik@outlook.com>
2018-02-08 21:02:27 -0600

    corrected ffmpeg script

--- Bash/mp4_360p_conversion.sh
+++ Bash/mp4_360p_conversion.sh
@@ -2,5 +2,5 @@
 
 while read -rd '' vidin; do
     vid="${vidin/IT_EDU/it_edu_vids}";
-    < /dev/null ffmpeg -i "$vidin" -vcodec hevc -preset slow -b:v 250k -maxrate 250k -bufsize 500k -vf scale=-1:360 -acodec aac -ab 96k "$vid";
+    </dev/null ffmpeg -i "$vidin" -c:v libx265 -crf 28 -b:v 250k -maxrate 250k -bufsize 500k -vf scale=-1:360 -acodec aac -ab 96k "$vid";
 done < <(find IT_EDU -name *.mp4 -print0)
