58eefd1b56a6fe6f0484e3d28e2fb465337b7f9d
Erik Morgan <morgan.erik@outlook.com>
2018-02-19 21:34:42 -0600

    js sort function complete

--- Web/pytor/static/scripts.js
+++ Web/pytor/static/scripts.js
@@ -1,70 +1,38 @@
 // manage qbt-cookie in python (look up timeout of webui cookie)
-let dls = document.getElementsByClassName('plus');
-for (dl of dls) {
-    dl.addEventListener('click', function (e) {
-        mag = e.target.getAttribute('data-mag');
-        // SEND TO /qbt ENDPOINT
+var addTorrent = (target) => {
+    target.addEventListener('click', (e) => {
+        let mag = e.target.getAttribute('data-mag');
         fetch('/qbt', {
             method: 'POST',
-            body: data
-            // OR '&#x2713'
-        }).then(response => e.target.innerText = String.fromCharCode(2713));
-        /*
-            USING WEBSOCKET WITH PYTHON HANDLING THIS STUFF:
-                Open the socket outsids of the loop
-            
-            var sock = new WebSocket('ws://localhost:##');
-            sock.onopen = function (event) {
-                sock.send(mag);
-            };
-            // attach this to window so socket closes when window closes
-            sock.close();
-        */
-    }
-}
+            body: mag
+        }).then(() => (e.target.innerText = String.fromCharCode(2713)));
+    });
+    return undefined;
+};
+Array.from(document.getElementsByClassName('plus')).map(addTorrent);
 
-// TABLE SORTING FUNCTION
-var headers = document.getElementsByTagName('th');
-for (var i = 0; i < headers.length - 1; i++) {
-    headers[i].addEventListener('click', function () {
-        let tbody = document.getElementById('results-body'),
-            rows = tbody.getElementsByTagName('tr'),
-            results = tbody.getElementsByTagName('tr'),
-            numRows = rows.length,
-            sorted = false,
-            dir = 1; // 1 = ascending && -1 = descending
-        while (!sorted) {
-            rows.sort(function (r1, r2) {
-                let n1 = r1.cells[0].innerHTML.toUpperCase(),
-                    n2 = r2.cells[0].innerHTML.toUpperCase();
-                if (n1 < n2) return dir * 1;
-                if (n1 > n2) return dir * -1;
-                return 0;
-            }
-            if (i) {
-                rows.sort(function (r1, r2) {
-                    let e1 = i < 2 ? r1.cells[i].getAttribute('data-bytes') : r1.cells[i].innerHTML,
-                        e2 = i < 2 ? r2.cells[i].getAttribute('data-bytes') : r2.cells[i].innerHTML;
-                    return dir * (parseInt(e1) - parseInt(e2));
-                })
-            }
-            for (var x = 0; x < numRows; x++) {
-                if (rows[x] !== results[x]) break;
-            }
-            if (x == numRows) {
-                dir *= -1;
-            } else {
-                sorted = true;
-            }
-        }
-        for (let r = 0; r < numRows - 1; r++) {
-            tbody.appendChild(rows[r]);
-        }
-    }
-}
+// if doing single-page updates, receive info as json, and create a map with data (mag hashes as keys)
+// ADD TITLES TO EVERY TNAME
+// CONSIDER FILTERING DUPLICATE NAME/SIZE IN ADDITION TO HASHES
+//     BC OF PERIOD/NON-PERIOD VARIATIONS & TITLES VS INNERHTML
+// TIME SORT USING FOREACH VS MAP ON LINE 23
 
-/*
-    JS For:
-        Download Buttons
-        Adding to qbittorrent
-*/
+var opts = { sensitivity: 'base', numeric: true },
+    table = document.querySelector('.results'),
+    compare = (a1, a2, ix) => a1[ix].localeCompare(a2[ix], undefined, opts);
+document.querySelectorAll('.head').forEach((head, i) => {
+    head.addEventListener('click', (e) => {
+        let rows = Array.from(document.querySelectorAll('.tor:not(.header)')),
+            rowData = rows.map((row, index) => [
+                row.children[0].innerHTML,
+                row.children[1].getAttribute('data-bytes'),
+                row.children[2].innerHTML,
+                index
+            ]),
+            dir = head.classList.contains('sorta') ? -1 : 1;
+        table.querySelector('[class*=sort]').classList.remove('sorta', 'sortd');
+        head.classList.add(dir > 0 ? 'sorta' : 'sortd');
+        rowData.sort((a, b) => compare(a, b, i) * dir || (i ? compare(a, b, 0) : compare(b, a, 2)));
+        rowData.map(row => table.appendChild(rows[row[3]]));
+    });
+});
