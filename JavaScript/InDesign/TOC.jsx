386d08522e38c12d01ba6f9d02d96341b5deeb8e
Erik Morgan <morgan.erik@outlook.com>
2018-03-08 21:41:17 -0600

    pypub update

--- JavaScript/InDesign/TOC.jsx
+++ JavaScript/InDesign/TOC.jsx
@@ -1,79 +1,1 @@
-#target indesign
-var doc = app.activeDocument, mainStory;
-
-clearBoxes();
-app.scriptPreferences.enableRedraw = false;
-main();
-app.scriptPreferences.enableRedraw = true;
-
-function main () {
-    mainStory = findChangeGrep({'findWhat': '(?i)suggested procedure\\r'})[0].parent;
-    var paras = mainStory.paragraphs.everyItem().getElements(),
-         mainFrame = mainStory.textContainers[0];
-    for (var p = numParas.length - 1; p > -1; p--) {
-        if (paras[p].appliedParagraphStyle.name[0] == 'L') {
-            var par = paras[p],
-                 pstyle = par.appliedParagraphStyle.name,
-                 pnum = par.parentTextFrames[0].parentPage.name;
-            par.contents = par.contents.replace(/ *\n */i, ' ');
-            par.insertionPoints[-2].contents = '\t' + pnum;
-            par.appliedParagraphStyle = pstyle.replace(/(Level \d).*/, 'TOC $1');
-        }
-        else if (paras[p].appliedParagraphStyle.name == 'SUGGESTED PROCEDURE') {
-            paras[p].insertionPoints[-1].contents = 'Table of Contents\r';
-            mainStory.paragraphs[paras.length - p - 1].applyParagraphStyle(doc.paragraphStyles.item('TOC'), true);
-            break;
-        }
-        else paras[p].remove();
-    }
-    mainStory.paragraphs.everyItem().clearOverrides();
-    findChangeGrep({'capitalization': Capitalization.NORMAL}, {'hyphenation': false, 'rightIndent': '0.375i', 'lastLineIndent': '0.125i'}, undefined);
-    doc.pages.itemByRange(1, doc.pages.length-1).remove();
-    var grps = doc.spreads[0].groups.everyItem().getElements();
-    for (var g = grps.length - 1; g > -1; g--) {
-        if (grps[g].allGraphics.length) grps[g].remove();
-    }
-    doc.sections.add(doc.pages.firstItem(), {pageNumberStyle:PageNumberStyle.lowerRoman});
-    findChangeGrep({'findWhat': 'Page ', 'appliedParagraphStyle': 'Page No.'}, {'changeTo': ''}, {'includeMasterPages': true});
-    mainStory.insertionPoints[-1].contents = 'Assembly Drawings and Parts Lists\r';
-    mainStory.paragraphs.lastItem().applyParagraphStyle('TOC Level 2', true);
-    mainStory.insertionPoints[-1].applyParagraphStyle(doc.paragraphStyles.item('TOC Parts List'), true);
-    app.menuActions.itemByName('Sort by Name').invoke();
-    mainFrame.properties = {geometricBounds: ['67.5pt', '72pt', '648pt', '558pt'], itemLayer: 'Default'};
-    if (mainFrame.overflows) handleOverflow();
-    else mainFrame.geometricBounds = ['67.5pt', '72pt', '720pt', '558pt'];
-}
-
-function handleOverflow () {
-    var plusFrame = doc.pages[0].textFrames.add('Default', {geometricBounds: ['684pt', '72pt', '720pt', '558pt'], contents: '+'});
-    plusFrame.parentStory.properties = ({appliedFont: 'ITC Bookman Std', fontStyle: 'Bold', pointSize: '24pt', justification: Justification.rightAlign, alignToBaseline: true});
-    var newPage = doc.pages.add(LocationOptions.AFTER, doc.pages.firstItem());
-    var newFrame = newPage.textFrames.add('Default', {geometricBounds: ['76.8pt', '54pt', '738pt', '540pt']});
-    tocFrame.nextTextFrame = newFrame;
-    if (mainStory.textContainers.length < 2) newFrame.previousTextFrame = mainStory.textContainers[0];
-}
-
-function clearBoxes(){
-    app.findChangeObjectOptions = app.findObjectPreferences = NothingEnum.nothing;
-    app.findObjectPreferences.properties = {fillColor: 'Black', strokeColor: 'None'};
-    app.findChangeObjectOptions.objectType = ObjectTypes.ALL_FRAMES_TYPE;
-    var boxes = doc.findObject();
-    for (b = 0; b < boxes.length; b++){
-        boxes[b].remove();
-    }
-}
-
-function findChangeGrep(findPrefs, changePrefs, findChangeOptions) {
-    app.findChangeGrepOptions = app.findGrepPreferences = app.changeGrepPreferences = NothingEnum.nothing;
-    if (findChangeOptions !== undefined) {
-        app.findChangeGrepOptions.properties = findChangeOptions;
-    }
-    app.findGrepPreferences.properties = findPrefs;
-    if (changePrefs == undefined) {
-        return doc.findGrep();
-    }
-    else {
-        app.changeGrepPreferences.properties = changePrefs;
-        doc.changeGrep();
-    }
-}
+#target indesignvar doc = app.activeDocument, mainStory, mainFrame;doc.viewPreferences.horizontalMeasurementUnits = doc.viewPreferences.verticalMeasurementUnits = MeasurementUnits.INCHES;clearBoxes();app.scriptPreferences.enableRedraw = false;main();app.scriptPreferences.enableRedraw = true;function main () {	mainStory = findChangeGrep({'findWhat': '(?i)suggested procedure\\r'})[0].parent;	mainFrame = mainStory.textContainers[0];	for (var p = mainStory.paragraphs.length - 1; p > -1; p--) {		var par = mainStory.paragraphs[p];		if (par.appliedParagraphStyle.name[0] == 'L') {			var pstyle = par.appliedParagraphStyle.name,				 pnum = par.parentTextFrames[0].parentPage.name;			par.characters[-2].insertionPoints[1].contents = '\t' + pnum;			par.applyParagraphStyle(doc.paragraphStyles.item(pstyle.replace(/(Level \d).*/, 'TOC $1')), true);			if (/Level [345]/i.test(pstyle)) par.properties = {hyphenation: false, rightIndent: '0.375i', lastLineIndent: '-0.125i'};		}		else if (par.appliedParagraphStyle.name == 'SUGGESTED PROCEDURE') {			par.insertionPoints[-1].contents = 'Table of Contents\r';			mainStory.paragraphs[p + 1].applyParagraphStyle(doc.paragraphStyles.item('TOC'), true);			break;		}		else par.remove();	}	findChangeGrep({'findWhat': ' *\\n *', 'appliedFont': 'ITC Bookman Std'}, {'changeTo': ' '}, undefined);	doc.pages.itemByRange(1, doc.pages.length-1).remove();	var grps = doc.spreads[0].groups.everyItem().getElements();	for (var g = grps.length - 1; g > -1; g--) {		if (grps[g].allGraphics.length) grps[g].remove();	}	doc.sections.add(doc.pages.firstItem(), {pageNumberStyle:PageNumberStyle.lowerRoman});	findChangeGrep({'findWhat': 'Page ', 'appliedParagraphStyle': 'Page No.'}, {'changeTo': ''}, {'includeMasterPages': true});	mainStory.insertionPoints[-1].contents = 'Assembly Drawings and Parts Lists\r';	mainStory.paragraphs.lastItem().applyParagraphStyle(doc.paragraphStyles.item('TOC Level 2'), true);	mainStory.insertionPoints[-1].applyParagraphStyle(doc.paragraphStyles.item('TOC Parts List'), true);	app.menuActions.itemByName('Sort by Name').invoke();	mainFrame.properties = {geometricBounds: ['67.5pt', '72pt', '648pt', '558pt'], itemLayer: 'Default'};	if (mainFrame.overflows) handleOverflow();	else mainFrame.geometricBounds = ['67.5pt', '72pt', '720pt', '558pt'];	for (var p = 0; p < mainStory.paragraphs.length; p++) {		var par = mainStory.paragraphs[p];		if (par.appliedParagraphStyle.name.substr(0, 3) == 'TOC' && par.words[-2].insertionPoints[-1].horizontalOffset - par.horizontalOffset > 4.375)			par.words[-2].insertionPoints[0].contents = '\n';	}	mainStory.insertionPoints[-1].applyParagraphStyle(doc.paragraphStyles.item('TOC Parts List'), true);}function handleOverflow () {	var plusFrame = doc.pages[0].textFrames.add('Default', {geometricBounds: ['684pt', '72pt', '720pt', '558pt'], contents: '+'});	plusFrame.parentStory.properties = ({appliedFont: 'ITC Bookman Std', fontStyle: 'Bold', pointSize: '24pt', justification: Justification.rightAlign, alignToBaseline: true});	var newPage = doc.pages.add(LocationOptions.AFTER, doc.pages.firstItem());	var newFrame = newPage.textFrames.add('Default', {geometricBounds: ['76.8pt', '54pt', '738pt', '540pt']});	mainFrame.nextTextFrame = newFrame;	if (mainStory.textContainers.length < 2) newFrame.previousTextFrame = mainStory.textContainers[0];}function clearBoxes(){	app.loadFindChangeQuery('NoteBox', SearchModes.OBJECT_SEARCH);	var boxes = doc.findObject();	for (b = 0; b < boxes.length; b++){		boxes[b].remove();	}}function findChangeGrep(findPrefs, changePrefs, findChangeOptions) {	app.findChangeGrepOptions = app.findGrepPreferences = app.changeGrepPreferences = NothingEnum.nothing;	if (findChangeOptions !== undefined) {		app.findChangeGrepOptions.properties = findChangeOptions;	}	app.findGrepPreferences.properties = findPrefs;	if (changePrefs == undefined) {		return doc.findGrep();	}	else {		app.changeGrepPreferences.properties = changePrefs;		doc.changeGrep();	}}
--- Python/pypub.py
+++ Python/pypub.py
@@ -4,6 +4,7 @@ import re
 import zipfile
 from lxml import etree
 
+# TODO: ADD TITLES TO RUNNING PROCEDURE LIBRARY AS METADATA
 dirs = {}
 
 def init_pub():
@@ -29,7 +30,7 @@ def getOutline():
     if len(docs) == 1:
         doc = docs[0]
     else:
-        doc = docs[0] if (re.search('(?i)a-z|pull', docs[1]) else docs[1]
+        doc = docs[0] if re.search('(?i)a-z|pull', docs[1]) else docs[1]
     parseOutline(zipfile.ZipFile(doc).open('word/document.xml').read())
 
 def parseOutline(xdoc):
@@ -37,10 +38,72 @@ def parseOutline(xdoc):
     # body contains paragraphs <w:p>
     # each paragraph contains runs of content <w:r>
     # runs have text inside them <w:t>
-    doc = etree.fromstring(xdoc)
+    #
+    # Delete everything before TABLE OF CONTENTS line.
+    # Delete paragraphs with strike-through.
+    # Delete everything after tab.
+    # First check for existence of all drawings (minus stack-ups)
+    # 
+    # save system, project, and rig from header
+    
+    doc = objectify(reformat(xdoc))
     
     
+
+def reformat(doc_str):
+    # check how word handles headers in xml
+    # use outline metadata for proj details
+    # urldecode special characters. see if lxml does it, or do global f/r
+    # see if para.text or para.text_content() works
+    xml = doc_str.replace('<w:tab/>', '<w:t>\\t</w:t>')
+    xml = xml.replace('</w:p>', '<w:r><w:t>\\r</w:t></w:r></w:p>')
+    doc = etree.fromstring(xml)
+    xns = {'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
+    paras = doc.xpath('//w:p[not(.//w:strike) and not(.//w:u and .//w:caps and .//w:b)]', namespaces=xns)
+    data = ['' if not p.text_content() else p.text_content() for p in paras]
     
-def cleanXML(doc):
-    for par in doc.iter('w:p'):
-    
+    for xpara in paras:
+        parText = 
+        for run in xpara.iter('w:r'):
+            if run[-1].tag is 'w:t':
+                parText += run[-1].text
+                if run[-1].tag is 'w:tab':
+                    break
+        if 'OUTLINE' in parText:
+            break
+            app(proj)
+        if re.search('(?i)order|due|deliver|engineer|by:', parText):
+            continue
+        proj += parText + ' '
+    paras = doc.xpath('//w:p[position()>' + (pindex + 1) + ']') #  and not(w:pPr/w:rPr/w:strike)
+    for para in paras:
+        # time difference if loop thru runs vs replacing tabs w/ txt
+        pstyle = etree.tostring(para[0][-1])
+        if 'w:strike' in pstyle or ('w:u' in pstyle and 'w:b' in pstyle):
+            continue
+        parText = ''
+        runs = para.xpath('//w:r[not(w:rPr/w:strike)]')
+        for run in runs:
+            if run[-1].tag is 'w:t':
+                parText += run[-1].text
+            elif run[-1].tag is 'w:tab':
+                parText += '\t'
+            
+        runs = xpara.xpath('//w:r[not(w:strike)]/w:t')
+        if runs:
+            paraText = ''
+            for run in runs:
+
+
+        if par.find('w:pPr')
+    rel = {
+        'system': doc[0],
+        'number': doc[0],
+        'customer': doc[0],
+        'project': doc[0],
+        'rig': doc[0],
+        'revision': doc[0],
+        'volume': doc[0]
+    }
+
+def objectify(docmap):
