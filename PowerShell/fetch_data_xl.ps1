01e1df832ae3402c28b3af63e16406f4fd2b3e20
Erik Morgan <morgan.erik@outlook.com>
2018-03-11 22:47:59 -0500

    update to dqscrape powershell

--- PowerShell/fetch_data_xl.ps1
+++ PowerShell/fetch_data_xl.ps1
@@ -64,11 +64,78 @@ Do {
 }
 Until ($dataRange -match '([A-Z]+[0-9]+:\1[0-9]+')
 
+Do {
+    $tableIndex = Read-Host -Prompt 'Index of table to scrape (one-based)'
+}
+Until ($tableIndex -gt 0)
+
 $xl = New-Object -comobject Excel.Application
 $xl.Visible = $false
 $wb = $xl.Workbooks.Open($doc)
 $ws = $wb.Worksheets.Item($sheetIndex)
 # $range = $ws.Range($dataRange)
-$data = $ws.Range($dataRange) | Select text
-ForEach ()
+$data = @($ws.Range($dataRange) | Select text)
+$dataHeader = $baseURL -replace '.+\?(\w+)=', '$1'
+$scraped = New-Object System.Collections.ArrayList
+foreach ($val in $data) {
+    $tableData = New-Object System.Collections.Specialized.OrderedDictionary
+    $html = (Invoke-WebRequest "$baseURL$val").Content -replace '<(area|base|col|embed|hr|img|input|link|meta|param|source|track|wbr)( [^>]+)?>', ''
+    [xml]$xdoc = $html -replace '&\S+;', ''
+    $ns = new-object Xml.XmlNamespaceManager $xdoc.NameTable
+    $ns.AddNamespace("ns", $xdoc.DocumentElement.Attributes.GetNamedItem('xmlns'))
+    $table = $xdoc.getElementsByTagName('table')[$tableIndex]
+    $headerClass = 'a'
+    $tableData.Add($dataHeader, $val)
+    $table.SelectNodes('.//ns:td[@class="a"]', $ns) | % {
+        $tableData[$_.innerText.Trim()] = $_.NextSibling.InnerText.Trim()
+    }
+    $scraped.Add($tableData)
+}
+# Test difference if using arraylist of hashtables vs arraylist vs array
+# $([xml]$web).html.body.table.tr[5].td[1].b
+# Add-Type -AssemblyName System.Xml.Linq
+# $txt=[IO.File]::ReadAllText("c:\myhtml.html")
+# $xml = [System.Xml.Linq.XDocument]::Parse($txt)
+# $ns='http://www.w3.org/1999/xhtml'
+# $divs=$cells = $xml.Descendants("{$ns}td")
+# 
+# USE XML!
+# html empty tags: area|base|br|col|embed|hr|img|input|link|meta|param|source|track|wbr
+# regex to find: <(area|base|br|col|embed|hr|img|input|link|meta|param|source|track|wbr)( [^>]+)?>
+
 $xl.quit()
+
+# OPTIONS:
+# 1. Get desired header values and use table.innerText + regex for data extraction
+# 2. Use class names or other attributes to specify header vs data cells
+# 3. Use colgroup and col tags
+# if ($table.getElementsByTagName('th').Count -eq 0 -And $table.getElementsByTagName('thead').Count -eq 0) {
+#     $headers = $table.SelectNodes('.//ns:td[not(@style)]')
+#     if ($headers.Count -eq 0) {
+#         Write-Host 'This html structure is really bad, and I need more information to get your data.'
+#         $headerPath = Read-Host -Prompt 'Enter the relative xpath for header elements (prefixed with "ns:"; eg ".//ns:td[@class="a"]")'
+#         $headerPath = Read-Host -Prompt 'Enter the relative xpath for data elements (prefixed with "ns:"; eg ".//ns:td[not(@class="a")]")'
+#         $headers = $table.SelectNodes($headerPath)
+#         if ($headers.Count -eq 0) {
+#             Write-Host 'Sorry, better luck next time. Exiting...'
+#             $xl.quit()
+#             Exit 1
+#         }
+#     }
+# }
+
+# $cells = @($row.Cells)
+# if ($cells[0].tagName -eq 'th') {
+#     $headers = @($cells | foreach { $_.innerText.Trim() })
+#     continue
+# }
+# $resultObject = [Ordered] @{}
+# for($counter = 0; $counter -lt $cells.Count; $counter++) {
+#     $title = $titles[$counter]
+#     if(-not $title) { continue }
+#     $resultObject[$title] = ("" + $cells[$counter].InnerText).Trim()
+# }
+# $headers = ($headerNodes | Select-Object -ExpandProperty InnerText).Trim()
+# foreach ($head in $headerNodes) {
+    
+# }
