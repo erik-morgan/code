5d3634cb90c2ddbb4789ff3a1ef6a3080f1dcaeb
Erik Morgan <morgan.erik@outlook.com>
2018-04-05 20:31:04 -0500

    pypub: pubdir + app progress

--- Python/pypub/pubdir.py
+++ Python/pypub/pubdir.py
@@ -1,14 +1,33 @@
-from tkinter import StringVar
-from pathlib import Path
+from tkinter import filedialog, StringVar
+from tkinter.ttk import Button, Entry, Label
 
 class PubDir:
     def __init__(self, dir_name):
-        self.str_var = StringVar()
-        self.dir_name = dir_name
+        self.strvar = StringVar()
+        self.name = dir_name
+    
+    def tkinit(self, parent, label, get_dir_cb):
+        self.label = Label(parent, text=label_text)
+        self.entry = Entry(parent, textvariable=self.strvar, state='readonly')
+        self.button = Button(parent, text='Browse')
+        self.button['command'] = self._get_dir(get_dir_cb)
+    
+    def tkstyle(self, widget, col, row):
+        pass
+    
+    def _get_dir(callback):
+        def pick_dir(self):
+            dir_path = filedialog.askdirectory()
+            if dir_path and dir_path != self.svar():
+                self.svar(dir_path)
+            callback()
+        return pick_dir
+    
+    def fsave(self):
+        return f'{self.name}={self.svar()}\n'
     
     def svar(self, svar_val=None):
         if svar_val:
-            self.str_var.set(svar_val)
-            setattr(self, 'path', Path(svar_val))
+            self.strvar.set(svar_val)
         else:
-            return self.str_var.get()
+            return self.strvar.get()
--- Python/pypub/pypub_app.py
+++ Python/pypub/pypub_app.py
@@ -1,14 +1,19 @@
 from pathlib import Path
 from tkinter import Tk, StringVar, ttk, filedialog
-import pypub
+from inspect import ismethod
+from pubdir import PubDir
 
 class Pypub:
-        
+    _state = {
+        True: 'normal',
+        False: 'disabled'
+    }
+    
     def __init__(self, parent):
-        self.indd = {'var': StringVar()}
-        self.pdfs = {'var': StringVar()}
-        self.draw = {'var': StringVar()}
-        self.proj = {'var': StringVar()}
+        self.indd = PubDir('indd')
+        self.pdfs = PubDir('pdfs')
+        self.draw = PubDir('draw')
+        self.proj = PubDir('proj')
         self.config = Path(__file__).parent.resolve() / 'config'
         if self.config.exists():
             self.load_dirs()
@@ -16,67 +21,65 @@ class Pypub:
         self.parent.title('pypub')
         init_gui()
     
-    def init_gui():
-        self.mainframe = ttk.Frame(self.parent, padding=24)
-        self.mainframe.grid(column=0, row=0, sticky='nesw')
-        self.mainframe.columnconfigure(0, weight=1)
-        self.mainframe.rowconfigure(0, weight=1)
-        indd_label = ttk.Label(mainframe, text='InDesign Folder:')
+    def init_gui(self):
+        self.frame = ttk.Frame(self.parent, padding=24)
+        self.frame.grid(column=0, row=0, sticky='nesw')
+        self.frame.columnconfigure(0, weight=1)
+        self.frame.rowconfigure(0, weight=1)
+        self.indd.tkinit(self.frame, 'InDesign Folder', can_run)
+        self.pdfs.tkinit(self.frame, 'PDFs Folder', can_run)
+        self.draw.tkinit(self.frame, 'Drawings Folder', can_run)
+        self.proj.tkinit(self.frame, 'Project Folder', can_run)
+        # 
+        indd_label = ttk.Label(self.frame, text='InDesign Folder:')
         indd_label.grid(column=0, row=0, sticky='w', padx=12)
-        indd_entry = ttk.Entry(mainframe, textvariable=self._indd, state='readonly')
+        indd_entry = ttk.Entry(self.frame, textvariable=self._indd, state='readonly')
         indd_entry.grid(column=1, row=0, columnspan=3, sticky='ew')
-        indd_button = ttk.Button(mainframe, text="Browse", command=set_indd)
+        indd_button = ttk.Button(self.frame, text='Browse', command=set_indd)
         indd_button.grid(column=4, row=0, sticky='we')
+        run_app = ttk.Button(self.frame, text='Run', command=run)
         # set minwidths on columns
     
-    def set_indd(self):
-        # check what happens if user cancels
-        # remember to check status of all entrys to enable run button
-        # and denote required fields with red asterisks, and put legend/note at bottom
-        self.indd.set(filedialog.askdirectory())
+    def run_app(self):
+        if not self.dirs or set(self.idir('svar')) != set(self.dirs.values()):
+            self.save_dirs()
     
-    def run(self):
-        # save on cancel as well
-        pass
+    def can_run():
+        run_bool = self.run_app['state'] == 'normal'
+        if all(self.idir('svar', True)) != run_bool:
+            self.run_app['state'] = self._state[not run_bool]
     
-    def loaddirs(self):
-        with self._config.open() as f:
-            items = [ln.split('=', maxsplit=1) for ln in f]
-        dirs = {k.strip():v.strip() for k, v in items}
-        self.indd['var'].set(dirs['indd'])
-        self.pdfs['var'].set(dirs['pdfs'])
-        self.draw['var'].set(dirs['draw'])
+    def load_dirs(self):
+        with self.config.open() as f:
+            lines = [ln.split('=', maxsplit=1) for ln in f]
+        self.dirs = {k.strip():v.strip() for k, v in lines}
+        for d in self.idir():
+            d.svar(self.dirs[d.name])
     
-    def savedirs(self):
-        # try this instead:
-        # dirs = {
-        #     'indd': {
-        #         'var': StringVar(),
-        #         'label': ,
-        #         'entry': ,
-        #         'button': 
-        #     }
-        # }
-        # dirs['indd']['var']
-        # 
-        # OR write quick little dir class
-        # 
-        # 
-        # 
-        items = '\n'.join('='.join(item) for item in self.dirs.items())
-        self._config.write_text(items)
+    def save_dirs(self):
+        self.config.write_text(''.join(list(self.idir('svar'))))
     
-    def make_style():
+    def idir(self, attr_name=None, incl_proj=False):
+        idirs = [self.indd, self.pdfs, self.draw]
+        if incl_proj:
+            idirs.append(self.proj)
+        for d in idirs:
+            if attr_name:
+                attrib = getattr(d, attr_name)
+                yield attrib() if ismethod(attrib) else attrib
+            else:
+                yield d
+    
+    def make_style(self):
         s = ttk.Style()
         s.theme_create('dq', parent = 'alt')
         s.theme_use('alt')
         s.configure('dq.TButton', borderwidth=0)
-
     
-    # pdfs_label = 
-    # draw_label = 
-    # proj_label = 
-    # for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
-    # feet_entry.focus()
-    # root.bind('<Return>', calculate)
-    # w.mainloop()
+#    def pick_dir(self, dir_obj):
+#        # check what happens if user cancels
+#        # remember to check status of all entrys to enable run button
+#        # and denote required fields with red asterisks, and put legend/note at bottom
+#        def ret_func():
+#            dir_obj.get_dir()
+#            if all(d.svar() for d in [self.indd, self.pdfs, self.draw, self.proj])
