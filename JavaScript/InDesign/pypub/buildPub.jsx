2b0c5e0890fc207d25cc83201f394eaa72fb9d92
Erik Morgan <morgan.erik@outlook.com>
2018-06-01 23:12:36 -0500

    pypub back to buildPub.jsx; global functions too complex with as/vbs; revised buildPub only needs single call per file

--- JavaScript/InDesign/pypub/buildPub.jsx
+++ JavaScript/InDesign/pypub/buildPub.jsx
@@ -1,35 +1,75 @@
 #target indesign
-#targetengine "pypub"
+
+/*
+ * in python, copy all indd/pdfs to a project folder, & copy source file into dir with TOC if no pdf is found
+ * 
+ * Might have to replace literals with non-literals (eg find \\t replace with \t)
+ * 
+ * See if revs in file names messes up any of the naming logic
+ * Check if doing SS0203-11.A.indd/SS0203-11.B.indd works for duplicate procedures
+ * Add python code to determine whether multiple procs are duplicates
+ *     (eg SS0284-02 always the same; SS0240 usually different)
+ * 
+ * DUE TO PYTHONS SUPERIOR LIST HANDLING, ORGANIZE DRAWINGS IN PYTHON, BEFORE SAVING PUBDATA
+ * 
+ * IN PYTHON, IF A BLUESHEET ITEM DOES NOT HAVE AN ID, GENERATE A RANDOM NUMBER
+ * REQUIRE BLUESHEETS TO BE FORMATTED ACCORDINGLY (NO BS ILLUSTRATIONS; UNLESS I USE BOLD FORMATTING)
+ * OR, USE MARKER ON ILLUSTRATIONS COMBINED WITH FORMATTING!!!
+ * STILL, MAKE LIFE EASIER AND LIST ILLUSTRATIONS SEPARATELY IN JSON
+ * 
+ * CLEAN DRAWING DESCRIPTIONS IN PYTHON & SAVE IN JSON FORMAT
+ */
+// ALSO HANDLE THIS REGEX: [/(\d+)k/ig, '$1k']
+// regexs = [[/\bmin.?\b/ig, 'Min.'],[/\bmax.?\b/ig, 'Max.'],[/\u2018|\u2019/ig, '\''],[/\u201C|\u201D/ig, '"'],[/\s*(\t|\r)+\s*/ig, '$1'],[/^(420(056|295)-02).*/ig, '$1\t18-3/4" Jet Sub'],[/(bb|bigbore).?(ii|2)/ig, 'BB II'],[/\bchsart\b/ig, 'Casing Hanger and Seal Assembly Running Tool'],[/\bsart\b/ig, 'Seal Assembly Running Tool'],[/-in\b/ig, '-In'],[/-out\b/ig, '-Out'],[/ & /ig, ' and '],[/mill and flush|m.?(&|and).?f/ig, 'Mill & Flush'],[/\bmpt\b/ig, 'Multi-Purpose Tool'],[/(three|3).?in.?(one|1)/ig, '3-in-1'],[/1st/ig, 'First'],[/2nd/ig, 'Second'],[/3rd/ig, 'Third'],[/\bpos\b/ig, 'Position'],[/\bolr\b/ig, 'Outer Lock Ring'],[/\bbr.style/ig, 'BR-Style'],[/f\/ ?/ig, 'for'],[/\b(.)x(.)/ig, '$1 x $2'],[/ {2,}/g, ' '],[/^\s|\s$/g, '']]
 
 prefs = {
     alerts: app.scriptPreferences.userInteractionLevel,
     preflight: app.preflightOptions.preflightOff,
     links: app.linkingPreferences.checkLinksAtOpen
 }
-
-app.scriptPreferences.properties = {
-    enableRedraw: false,
-    measurementUnit: MeasurementUnits.INCHES,
-    userInteractionLevel: UserInteractionLevels.NEVER_INTERACT
-};
+// DO PREFS IN AS/VBS
+app.scriptPreferences.enableRedraw = false;
+app.scriptPreferences.measurementUnit = MeasurementUnits.INCHES;
+app.scriptPreferences.userInteractionLevel = UserInteractionLevels.NEVER_INTERACT;
 app.preflightOptions.preflightOff = true;
 app.linkingPreferences.checkLinksAtOpen = false;
 
-cleanUp = function () {
-    app.scriptPreferences.properties = {
-        enableRedraw: true,
-        userInteractionLevel: prefs.alerts
-    };
+main();
+
+/*
+ * @param {String} docPath 
+ * IF DOCPATH IS SOURCE
+ * @param {Boolean} [exportDoc] 
+ * @param {Boolean} [createTOC] 
+ * @param {String} [drawList] 
+ * 
+ * IF DOCPATH IS TOC
+ * @param {String} [drawList] 
+ */
+
+function main () {
+    docPath = arguments[0];
+    openDoc(docPath);
+    if (docPath.indexOf('.TOC') > -1)
+        
+    if (exportDoc)
+        doc.exportFile(ExportFormat.PDF_TYPE, File(docPath.slice(0, -4) + 'pdf'), false);
+    if (drawList && drawList.length)
+    if (!/\bTOC\b/.test(docPath))
+        makeTOC
+    app.scriptPreferences.enableRedraw = true;
+    app.scriptPreferences.userInteractionLevel = prefs.alerts;
     app.preflightOptions.preflightOff = prefs.preflight;
     app.linkingPreferences.checkLinksAtOpen = prefs.links;
-    openDoc = grep = makeTOC = insertDrawings = styleTOC = null;
+    
 }
 
-openDoc = function (docPath) {
+function openDoc () {
     doc = app.open(File(docPath), false);
+    doc.viewPreferences.horizontalMeasurementUnits = doc.viewPreferences.verticalMeasurementUnits = MeasurementUnits.INCHES;
     if (!doc.saved) {
         for (var l = 0; l < doc.links.length; l++) {
-            // try status == 'lood'
+            // try status == 'lood' for link out of date
             if (doc.links[l].status == LinkStatus.LINK_MISSING) {
                 path = doc.links[l].filePath.replace(/.+?\/(?=share)/i, '');
                 path = path.replace(/\uF021/g, '*');
@@ -49,15 +89,7 @@ openDoc = function (docPath) {
     })[0];
 }
 
-grep = function (findPrefs, changePrefs, opts) {
-    app.findChangeGrepOptions = app.findGrepPreferences = app.changeGrepPreferences = null;
-    app.findChangeGrepOptions.properties = opts ? opts : {};
-    app.findGrepPreferences.properties = findPrefs;
-    app.changeGrepPreferences.properties = changePrefs ? changePrefs : {};
-    return changePrefs ? doc.changeGrep() : doc.findGrep();
-}
-
-makeTOC = function () {
+function makeTOC () {
     for (var p = mainStory.paragraphs.length - 1; p > -1; p--) {
         var para = mainStory.paragraphs[p],
             style = para.appliedParagraphStyle;
@@ -86,20 +118,42 @@ makeTOC = function () {
     doc.save(File(doc.fullName.absoluteURI.replace(/\.(?=indd$)/i, '.TOC.')));
 }
 
-insertDrawings = function (draws) {
-    if (Object.prototype.toString.call(draws).slice(8, -1) !== 'Array')
-        draws = draws.split('\r');
-    grep({findWhat: '(?i)^(Illustrat|Assembly Draw)[\\S\\s]+'}, {changeTo: drawText, appliedParagraphStyle: 'TOC Parts List'});
+function enterDrawings (drawingText) {
+    grep({findWhat: '(?i)^(Illustr|Assembly)[\\S\\s]+'}, {changeTo: drawingText});
+    var tocParas = grep({findWhat: '(?<=Contents\\r)[\\S\\s]+'})[0].paragraphs.everyItem().getElements();
+    for (var p = 0; p < tocParas.length; p++) {
+        var tocPara = tocParas[p],
+            paraParts = tocPara.contents.replace(/ {2,}|\n/g, ' ').split('\t');
+        /*
+         * Handle toc, drawing, and drawing title paras
+         * apply keep lines together ...
+         * 
+         */
+        if (paraParts.length == 1)
+            tocPara.appliedParagraphStyle = 'TOC Level 2';
+        else if (paraParts[1].length < 3)
+            // then its a toc paragraph
+        else if (/\d{5}/.test(paraParts[0]))
+            // then its a drawing
+        tocPara.keepAllLinesTogether = true;
+        if (tocPara.words[-1].horizontalOffset - tocPara.words[-2].insertionPoints[-1].horizontalOffset < 0.25)
+            tocPara.words[-2].insertionPoints[0].contents = '\n';
+    }
+    
 }
 
-styleTOC = function () {
-    doc.viewPreferences.horizontalMeasurementUnits = doc.viewPreferences.verticalMeasurementUnits = MeasurementUnits.INCHES;
-    doc.paragraphStyles.itemByRange('TOC Level 2', 'TOC Level 5').everyItem().properties = {keepAllLinesTogether: true, keepWithNext: 0, hyphenation: false, rightIndent: 0.5, lastLineIndent: -0.375};
-    doc.paragraphStyles.item('TOC Parts List').properties = {hyphenation: false, firstLineIndent: -1.75};
-    doc.paragraphStyles.item('TOC Parts List').tabStops[0].position = 3.5;
-    grep({findWhat: '([ \\n]+)'}, {changeTo: ' '});
-    grep({findWhat: '([ \\t]+)'}, {changeTo: '\\t'});
-    grep({findWhat: '^(Illustrat|Assembly Draw).+\\r'}, {appliedParagraphStyle: 'TOC Level 2', keepWithNext: 0});
+function insertDrawings (drawings) {
+    styleTOC();
+}
+
+function styleTOC () {
+
+    doc.paragraphStyles.itemByRange('TOC Level 2', 'TOC Level 5').everyItem().properties = {keepWithNext: 0, hyphenation: false, rightIndent: 0.5, lastLineIndent: -0.375};
+    doc.paragraphStyles.item('TOC Parts List').properties = {hyphenation: false, firstLineIndent: -1.75, tabList: [{position: 3.5, alignment: TabStopAlignment.LEFT_ALIGN}]};
+    grep({findWhat: ' {2,}|\\n'}, {changeTo: ' '});
+    grep({findWhat: '[ \\t]+'}, {changeTo: '\\t'});
+    grep({findWhat: '^\\s+|\\s+$'}, {changeTo: ''}, {includeMasterPages: true});
+    grep({findWhat: '^(Illustrat|Assembly Draw).+\\r'}, {appliedParagraphStyle: 'TOC Level 2', keepWithNext: 1});
     var tocParas = grep({findWhat: '(.+\\t\\d+\\r)+'})[0].paragraphs.everyItem().getElements();
     for (var p = 0; p < tocParas.length; p++) {
         var tocPara = tocParas[p];
@@ -121,7 +175,7 @@ styleTOC = function () {
     doc.save();
 }
 
-addBookmark = function () {
+function addBookmark () {
     doc.bookmarks.everyItem().remove();
     var titleParas = grep({findWhat: '(?i)\\A[\\S\\s]+\\r(?=suggest|table)'})[0].paragraphs,
         title = '';
@@ -132,7 +186,7 @@ addBookmark = function () {
             return match.slice(0, -3) + (/(activat|chang|driv|ma[kt]|operat|releas|replac|retriev|servic|terminat|us)ing/i.test(match) ? 'e' : '');
         });
     title = titleParas.itemByRange(title ? 1 : 0, -1).contents.replace(/\s*(\r|\n)\s*/g, ' ');
-    title =  title.replace(/\b[A-Za-z]+\b/g, function (match) {
+    title =  title.replace(/\b[a-z]+\b/ig, function (match) {
         if (/^(a|about|above|across|after|along|and?|around|a[st]|before|behind|below|between|but|by|for|from|in|into|like|mid|near|next|nor|o[fnr]|onto|out|over|past|per|plus|save|so|than|then?|till?|to|under|until|unto|upon|via|with|within|without|x|yet)$/i.test(match))
             return match.toLowerCase();
         if (/^(i{2,3}|PSI|BOP|HPHT|HTHL|HCLD|MPT|CADA|DWHC|ROV|SS|BR|DW)$|^[^aeiou]+)$/i.test(match))
@@ -141,4 +195,12 @@ addBookmark = function () {
     });
     app.panels.item('Bookmarks').visible = false;
     doc.bookmarks.add(doc.pages[0], {name: title});
+}
+
+function grep (findPrefs, changePrefs, opts) {
+    app.findChangeGrepOptions = app.findGrepPreferences = app.changeGrepPreferences = null;
+    app.findChangeGrepOptions.properties = opts ? opts : {};
+    app.findGrepPreferences.properties = findPrefs;
+    app.changeGrepPreferences.properties = changePrefs ? changePrefs : {};
+    return changePrefs ? doc.changeGrep() : doc.findGrep();
 }
