1836e8be9c2f3863e72774d6f978c120440af333
Erik Morgan <morgan.erik@outlook.com>
2018-06-11 20:54:44 -0500

    pypub: tested outlineparser < 1s; added pstyle support to outline parser

--- Python/pypub/outlineparser.py
+++ Python/pypub/outlineparser.py
@@ -1,38 +1,104 @@
+from zipfile import ZipFile
 import re
 from lxml import etree
 
 ############################################################
-# TODO: Add support for roman numeral volume numbers
-# REQUIRES PROJECT: TO BE ITS OWN LINE
-# CONSIDER IGNORING REVS BC IT SHOULD USE MAIN LIBRARY &
-# THERE ARE CONSTANT TYPOS, BUT IT WOULD REQUIRE NEW TOCS EVERY TIME
+# REQUIRES PROJECT TO BE ITS OWN LINE
+# CONSIDER: USE MAIN LIB & IGNORE REVS/TYPOS (NO SAVING TOCS)
 # Use 2-digit revs, even for nc (00)?; return else '00' for NC in future in _rev_repl function
 # standardize drawing descriptions and attach as metadata to drawing library files
 # was going to convert volume ints to roman numerals, but user needs to use correct, desired format
 # forbid "section" bluesheets; put the bluesheet next to the actual document
-#     investigate how bluesheets were denoted in that crazy outline that was missing like 6 procs
-############################################################
-# 
-# RESUME AT ADDPROC/ADDDRAW
-# 
+
+# ideally, figure out a more reliable way to remove namespaces
+# have to do styles bc of strikethru
 ############################################################
 
 class OutlineParser:
-    def __init__(self, xdoc):
-        if isinstance(xdoc, bytes):
-            xdoc = xdoc.decode()
-        xdoc = re.sub(r'\b[rw]:| xmlns[^>]+| encoding=\S+', '', xdoc)
+    def __init__(self, outline):
+        with ZipFile(outline) as zip:
+            xdoc = zip.open('word/document.xml').read().decode()
+            xstyles = zip.open('word/styles.xml').read().decode()
+        xdoc = self.loadStyles(xdoc, xstyles)
         self.doc = etree.fromstring(xdoc)[0]
+    
+    def loadStyles(self, xdoc, xstyles):
+        xstyles = re.sub(r'\b(r|w[a-z0-9]*):(?! )| xmlns[^>]+| encoding=\S+', '', xstyles)
+        xdoc = re.sub(r'\b(r|w[a-z0-9]*):(?! )| xmlns[^>]+| encoding=\S+', '', xdoc)
+        sdoc = etree.fromstring(xstyles)
+        for s in sdoc.iter('style'):
+            sid = s.get('styleId')
+            sprops = ''
+            stype = 'pStyle' if s.get('type') == 'paragraph' else 'rStyle'
+            for pr in s.iter('pPr', 'rPr'):
+                for child in pr:
+                    sprops += etree.tostring(child).decode()
+            xdoc = xdoc.replace(f'<{stype} val="{sid}"/>', sprops)
+        return xdoc
     
     def parse(self):
         self.getProps()
+        phase = ''
+        paragen = self.addPara()
+        next(paragen)
         for para in self.doc.iter('p'):
             if list(para.iter('strike')):
                 continue
-            if all(list(para.iter(tag)) for tag in 'ubt'):
-                self.addSect(para)
-            else:
-                self.addPara(self.getText(para))
+            ishead = all(list(para.iter(tag)) for tag in 'ubt')
+            if ishead and not list(para.getnext().iter('t')):
+                phase = re.sub(r'(?i) PHASE| PROCEDURES?|\t.*', '',
+                    self.getText(para))
+                if 'APPENDIX' in phase.upper():
+                    self.pub.set('appendix', 'True')
+                if phase.upper()[0:5] not in 'TABLE INTRO APPEN':
+                    etree.SubElement(self.pub, 'phase', {'title': phase})
+            elif phase and phase.upper()[0:5] not in 'TABLE INTRO APPEN':
+                if ishead:
+                    etree.SubElement(self.pub[-1], 'unit')
+                else:
+                    paragen.send(self.getText(para))
+    
+    def addPara(self):
+        while True:
+            para = yield
+            if re.search(r'\d{5}', para):
+                self.addDraw(para)
+            elif para.lower().startswith('rev') and not 'NC' in para.upper():
+                proc.set('rev', para.split()[-1])
+            elif 'advisory' in para.lower():
+                proc.set('advisory', 'True')
+            elif re.match(r'[A-Z]{2,6} ?\d+', para):
+                proc = self.addProc(para)
+    
+    def addDraw(self, text):
+        draw = {}
+        if self.draft and 'bluesheet' in text.lower():
+            draw['bs'] = 'True'
+        text = text.split('\t', 1)
+        draw['id'] = text[0].replace(' ', '')
+        if any(ch.isalpha() for ch in text[0]):
+            draw = etree.SubElement(self.pub[-1][-1], 'il', draw)
+        else:
+            draw = etree.SubElement(self.pub[-1][-1], 'dw', draw)
+        if len(text) > 1:
+            draw.text = text[1]
+    
+    def addProc(self, text):
+        proc = {}
+        if 'bluesheet' in text.lower():
+            proc['bs'] = 'True'
+        if 'rev' in text.lower() and not 'NC' in text.upper():
+            proc['rev'] = re.search(r'(?i)rev (\d+)', text)(1)
+        text = text.split('\t')
+        if 'BTC' in text[0].upper():
+            self.pub[-1][-1].set('title', 'BOP Test Options')
+            proc['id'] = re.sub(r'(?i)(BTC) ?(\d+).*', r'\1\2', text[0])
+        else:
+            proc['id'] = re.sub(r'\W', '', text[0])
+        proc = etree.SubElement(self.pub[-1][-1], 'rp', proc)
+        if len(text) > 1:
+            proc.text = text[1]
+        return proc
     
     def getProps(self):
         props = {}
@@ -48,62 +114,21 @@ class OutlineParser:
                     props['rev'] = re.search(r'Rev\S* (\d+)', prop, re.I)[1]
                 if 'vol' in prop.lower():
                     props['vol'] = re.search(r'Vol\S* (\S+)', prop, re.I)[1]
-                self.draft = props['draft'] = 'draft' in prop.lower()
+                self.draft = 'draft' in prop.lower()
+                props['draft'] = str(self.draft)
         self.pub = etree.Element('project', props)
-    
-    def addSect(self, sect):
-        text = self.getText(sect)
-        if sect[0][0:5] in 'TABLE INTRO':
-            return None
-        if list(sect.getnext().iter('t')):
-            return etree.SubElement(self.pub[-1], 'unit', {'title': text})
-        else:
-            return etree.SubElement(self.pub, 'phase', {
-                'title': re.sub(r' PHASE| PROCEDURES?|\t.*)', '', text)
-            })
-    
-    def addPara(self):
-        while True:
-            para = yield
-            if re.search(r'\d{5}', para):
-                self.addDraw(para)
-            elif para.lower().startswith('rev') and not 'NC' in para.upper():
-                proc.set('rev', para.split()[-1])
-            elif 'advisory' in para.lower():
-                proc.set('advisory', 'True')
-            elif re.match(r'[A-Z]{2,6} ?\d+', para):
-                proc = self.addProc(para)
-    
-    def addDraw(self, text):
-        # differentiate bw ills/draws
-        draw = {'id': tsplit[0]}
-        if self.draft and 'bluesheet' in para.lower():
-            draw['bs'] = 'True'
-        # self.docList.append(tsplit[0])
-        draw = etree.SubElement(self.pub[-1][-1], 'dw', draw)
-        draw.text = tsplit[1]
-    
-    def addProc(self, text):
-        # para = para.replace('BTC ', 'BTC')
-        # rev = re.sub(r'(?is)(.+\brev ?(\d+))?.*', r'\2', text)
-        # CHANGE HOW ID IS PICKED; PROBABLY INSIST ON -## ONLY, NOT -MT
-        # CHECK FOR INLINE REV (IE BTC)
-        proc = {
-            'id': '-'.join(re.split(r'\W', text.split()[0])),
-        }
-        if 'bluesheet' in text.lower():
-            proc['bs'] = 'True'
-        # self.docList.append(proc['id'] + (f'.R{rev}' if rev else ''))
-        return etree.SubElement(self.pub[-1][-1], 'rp', proc)
     
     def getText(self, p):
         if isinstance(p, int):
             p = self.doc[p]
+        paraText = ''
         for node in p.iter('t', 'tab', 'br'):
+            if node.getparent().tag != 'r':
+                continue
             if node.tag == 'br':
                 break
             if node.tag == 't':
-                paraText = (paraText or '') + node.text
+                paraText += node.text
             elif node.tag == 'tab' and paraText[-1] != '\t':
                 paraText = paraText.strip() + '\t'
         return paraText.strip() if paraText else ''
--- Python/pypub/pypub.new.py
+++ Python/pypub/pypub.new.py
@@ -1,17 +1,15 @@
 from observer import Observer
 from os import scandir
-from os.path import join, getmtime
 from lxml import etree
 from zipfile import ZipFile
 from fnmatch import fnfilter
 from outlineparser import OutlineParser
-from pub_exceptions import OutlineError, MissingFileError, AppendixError
+from pub_exceptions import OutlineError
 
 # if there are no drawings matching with PL, try with just drawnum for drawings with -0x charts
 # standardize RP naming convention (regarding CC0104-MT vs CC0104-QTM-CR vs CC0104-01MT)
 # TODO: if folders are on network, do os test to set network volume prefix (eg /Volumes vs N:/)
-# TODO: introductions and pdf cover locations
-# TODO: add intros and back cover to PDF location
+# TODO: intros and pdf/back cover locations (move to PDFs?)
 # to order SUDs for buildpub, just call illustration and drawing xpaths separately
 # 
 # IMPORTANT:
@@ -32,18 +30,11 @@ class Pypub(Observer):
         else:
             self.docx = self.docx[0]
     
-    def initParser(self):
+    def parseOutline(self):
         with ZipFile(self.docx) as zip:
             xdoc = zip.open('word/document.xml').read()
-        self.parser = OutlineParser(xdoc)
-    
-    def parseOutline(self):
-        for sect in self.parser.sections:
-            if 'APPENDIX' in sect[0]:
-                self.parser.doc.set('appendix', 'True')
-            else:
-                self.parser.addSect(sect)
-        self.pub = self.parser.xpub
+        parser = OutlineParser(xdoc)
+        self.pub = parser.parse()
     
     def dirlist(self, dirpath, pattern=None):
         def fileGen(dir):
