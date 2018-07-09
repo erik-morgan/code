36cbfd1bd3f0caae3d0bc88f3394f32e069a9090
Erik Morgan <morgan.erik@outlook.com>
2018-07-08 23:03:18 -0500

    progressWindow refactor complete except for testing, exit, & error handling; next refactor processINDD & procedure

--- JavaScript/ScriptUI/progress.jsx
+++ JavaScript/ScriptUI/progress.jsx
@@ -1,19 +1,81 @@
-﻿function ProgressWindow (title) {
+﻿/*
+ * inspect object references (eg in cancel onclick, use itself as base object)
+ * try to avoid closures unless they're needed later anyway
+It’s never a good idea to mix values of different types (e.g. numbers, strings, undefined or true/false) in the same array
+On the use of delete - Marc is right for global variables, I usually store the window object and related functions within one single object which itself is stored in the global variable. This way the global namespace is less cluttered, resulting in more speed improvements, and delete will work on the slots of that object.
+Indeed each library will add one (workspace) as soon as functions are loaded in the related closures (i.e. the INNER local variable and the SELF argument). That's the cost of the pattern, I agree, but it doesn't seem prohibitive so far, since each module is loaded once per session and never duplicated. About performance issues, I've always found they were negligible compared to DOM access time and ScriptUI LayoutManager intrinsic latency! However, when a INNER.xxx() or a SELF.yyy() routine needs to be invoked within a huge loop, you are right that the slowdown is noticeable. In such case, it's always better to create in the client code a local func variable that references the required method, then to call func() from within the loop.
+check out how estk's jsx files determine available engines & dollar sign setup
+
+add unwatch function in cleanup
+i can assigns w.texts.1, w.texts.2, etc for integer keys
+
+Indeed. Also, a slightly more succinct fix for IE6/7 would be:
+  function foo(element, a, b) {
+    element.onclick = function() {
+        // uses a and b
+    };
+    element = null; // avoid memory leak
+  }
+The closure contains a reference to foo's activation object, not to its properties. As a consequence, you can clear out the element reference after the event handler has been defined and this will break the circular reference.
+The above technique is also useful when creating anonymous functions for other purposes when element references happen to be lying around.
+
+
+BAD:
+var quantaty = 5;
+function addGlobalQueryOnClick(linkRef){
+    if(linkRef){
+        linkRef.onclick = function(){
+            this.href += ('?quantaty='+escape(quantaty));
+            return true;
+        };
+    }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////
+function ExampleConst(param){
+    this.method1 = function(){};
+    this.method2 = function(){};
+    this.method3 = function(){};
+    this.publicProp = param;
+}
+
+GOOD:
+var quantaty = 5;
+function addGlobalQueryOnClick(linkRef){
+    if(linkRef)
+        linkRef.onclick = forAddQueryOnClick;
+}
+function forAddQueryOnClick(){
+    this.href += ('?quantaty='+escape(quantaty));
+    return true;
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////
+function ExampleConst(param){
+    this.publicProp = param;
+}
+ExampleConst.prototype.method1 = function(){};
+ExampleConst.prototype.method2 = function(){};
+ExampleConst.prototype.method3 = function(){};
+
+*/
+
+function ProgressWindow (title) {
     this.window = new Window('palette', '    ' + title);
     this.count = 0;
     this.file = this.timer = this.eta = '';
-    this.register = function (item) {
-        var props = item.props.slice();
-        for (var p = 0; p < props.length; p++) {
-            this.watch(props[p], function (name, prev, next) {
-                return (item.props[p] = next);
-            });
-        }
-        item.watch('props', function (name, prev, next) {
-            item.text = localize.apply(undefined, [item.pattern].concat(next));
-            return next;
+}
+
+// in onShow, loop children, & if text, add watchers; use propNames for names, pattern for pattern, & props for props
+function textFactory (item, obj) {
+    var props = item.props.slice();
+    for (var p = 0; p < props.length; p++) {
+        obj.watch(props[p], function (name, prev, next) {
+            return (item.props[p] = next);
         });
-    };
+    }
+    item.watch('props', function (name, prev, next) {
+        item.text = localize.apply(undefined, [item.pattern].concat(next));
+        return next;
+    });
 }
 
 ProgressWindow.prototype.build = function (max) {
@@ -23,7 +85,8 @@ ProgressWindow.prototype.build = function (max) {
     
     // ctrl = this.window.add('statictext {characters: 80}');
     ctrl = this.window.add('statictext');
-    ctrl.props = ['Processing File %1/%2', 'count', 'total'];
+    ctrl.pattern = 'Processing File %1/%2';
+    ctrl.props = [, 'count', 'total'];
     this.register(ctrl);
     
     this.bar = getBar(this.window);
@@ -42,15 +105,13 @@ ProgressWindow.prototype.build = function (max) {
     this.register(ctrl);
     
     this.window.add('button', undefined, 'Abort', {name: 'cancel'});
-    this.window.cancel.onClick = this.close;
+    this.window.cancel.onClick = this.window.close;
     
     this.window.onClose = function () {
-        // return result of confirmation
-        this.aborted = true;
+        if (alert('Abort the operation?', true))
+            throw Error('User aborted the operation');
     };
     
-    this.aborted = false;
-    this.toClose = false;
     this.start = new Date();
     this.window.show();
 }
@@ -58,22 +119,11 @@ ProgressWindow.prototype.build = function (max) {
 /*
  * i want to be able to show it before beginning the loop
  * SEPARATE THIS.UPDATE FROM THIS.REFRESH: this allows refreshing the times inbetween Procedure method calls without updating the counter
- * add onChange handler to progressbar to set up onDraw vars
- * hide, close, then nullify a palette window (see bottom)
+ * wrap main loop in try/catch; the user cancelled
+ * MAKE SURE TO CALL PW.CLOSE IN FINALLY BLOCK
  * .visible || .show()
+ * add 'Operation cancelled by user, ending process...' notification on cancel
  * 
- * ANOTHER OPTION: pass ProgressWindow the structure (?) & a callback, so it does the looping:
- *     receive the array in build and process it
- *     for (var n = 0; n < array.length; n++) {
- *         if (!pb.aborted)
- *             doSomethingCallback(array[n]);
- *     }
- * 
- * OR: since scriptui can only process update events during execution, exclude a cancel button.
- *     rely on indesign's ability to cancel script with escape key, but will not close the dialog,
- *     so I'd have to devise a way of detecting the window (after script execution?), and closing it
- * 
- * perform try/catch in update? figure out how to interrupt the process when cancel is clicked
  * IMPORTANT: do testing in indd as well as estk, bc it often behaves differently in one vs the other
  */
 
@@ -82,13 +132,14 @@ ProgressWindow.prototype.update = function (name) {
     this.bar.value = ++this.count;
     this.timer = doTime((new Date() - this.start) / 1000);
     this.eta = doTime((obj.total-obj.count)*(obj.timer/obj.count));
-    if (!this.window.visible)
-        this.window.show();
 };
 
 ProgressWindow.prototype.close = function () {
+    this.window.onClose = null;
     this.window.close();
     this.window = null;
+    delete this.window;
+    // delete this? investigate persistence & reasons for it
 };
 
 function doTime (time) {
@@ -110,72 +161,21 @@ function getBar (parent) {
         fillBrush = g.newBrush(g.BrushType.SOLID_COLOR, [0.13, 0.59, 0.95]),
         pen = g.newPen(g.PenType.SOLID_COLOR, [1, 1, 1], 1);
     bar.preferredSize = [sz[0] * 60, sz[1] * 2];
+    bar.onChange = function () {
+        this.fraction = this.value / this.maxvalue;
+        this.text = (this.fraction * 100).toFixed(2) + '%';
+    };
     bar.onDraw = function () {
         var w = (this.size || this.preferredSize).width,
             h = (this.size || this.preferredSize).height,
-            ratio = this.value / this.maxvalue, s,
-            wt = g.measureString(s = (ratio * 100 ).toFixed(2) + '%')[0];
+            wt = g.measureString(this.text || '0%')[0];
         g.rectPath(0, 0, w, h),
         g.fillPath(backBrush);
         g.closePath();
         g.newPath();
-        g.rectPath(0, 0, ratio * w, h);
+        g.rectPath(0, 0, (this.fraction || 0) * w, h);
         g.fillPath(fillBrush);
-        g.drawString(s, pen, (w - wt) / 2, h / 4);
+        g.drawString(this.text, pen, (w - wt) / 2, h / 4);
     };
     return bar;
 }
-
-/*
-///////////////////////////////////////////////////////////////////////////////////////////////////
-dlg.cancel.onClick = dlg.close;
-dlg.onClose = function() { this.aborted = true; }
-dlg.aborted = dlg.toClose = false;
-dlg.center();
-dlg.show();
-const kLaunchTimeout = 300000; // wait until launched, timeout after 5 Min.
-var startTime = new Date();
-while( !dlg.aborted && !dlg.toClose ) {
-    try {
-        var task = this.target.cdic.isTargetRunning( this.target.address );
-        var res = CDICManager.getSynchronousResult( cdicMgr.callSynchronous( task ) );
-        if( res && res.length && res[0] )
-            dlg.toClose = true; // app is running
-        // pump application event loop several times to receive UI events
-        var abortIm = false;
-        for( var pumpLoop=0; pumpLoop<10 && !abortIm; pumpLoop++ )
-            abortIm = !app.pumpEventLoop();
-        if( abortIm ) {
-            dlg.aborted = true; // abort
-            dlg.close();
-            if( abortIm ) return; // estk is quitting
-        }
-    } catch( exc ) {
-        dlg.aborted = true; // abort
-        dlg.close();
-    }
-    var now = new Date();
-    dlg.aborted = ( dlg.aborted ? dlg.aborted : ( ( now - startTime ) > kLaunchTimeout ) );
-}
-var dlgaborted = dlg.aborted;
-dlg.close();
-dlg = null;     // force deletion of core Window element
-this.cb ? this.cb.call( !dlgaborted ) : app.launchError( this );
-///////////////////////////////////////////////////////////////////////////////////////////////////
-#targetengine ManageSingletonPalette
-$.UI || ($.UI = function F(FLAG) { // -1 > destroy; 0 > restore; 1  > rebuild
-    F.W || (F.W=Window.find("palette", "My Palette"));
-    if( F.W && FLAG ) {
-        F.W.visible && F.W.close();
-        F.W = null;
-        delete F.W;
-    }
-    if( -1===FLAG ) return;
-    if( !F.W ) {
-        F.W = new Window('palette', "My Palette", [50,50,300,300], {resizeable:true});
-        // etc.
-    }
-    F.W.visible || F.W.show();
-});
-///////////////////////////////////////////////////////////////////////////////////////////////////
-*/
---
+++ JavaScript/ScriptUI/progressWindow.jsx
@@ -0,0 +1,116 @@
+﻿#target indesign
+
+function ProgressWindow (title, max) {
+    var w = new Window('palette', '    ' + title);
+    this.count = 0;
+    this.file = this.tpass = this.tleft = '';
+    this.texts = {};
+    w.alignChildren = 'fill';
+    
+    this.texts.count = w.add('statictext');
+    this.texts.count.pattern = 'Processing File {count}/' + max;
+    
+    this.bar = buildBar(w);
+    this.bar.maxvalue = this.total = max;
+    
+    this.texts.file = w.add('statictext');
+    this.texts.file.pattern = 'Filename: {file}';
+    
+    this.texts.tpass = w.add('statictext');
+    this.texts.tpass.pattern = 'Time Elapsed: {tpass}';
+    
+    this.texts.tleft = w.add('statictext');
+    this.texts.tleft.pattern = 'Time Remaining: About {tleft}';
+    
+    w.add('button', undefined, 'Abort', {name: 'cancel'});
+    w.cancel.onClick = w.close;
+    
+    w.onClose = function () {
+        /* Options for cancelling:
+         * if alert, call onError that calls PW.close (but activation obj would still be in stack)
+         * object.watch this.aborted property
+         * put this into init method, and wrap it all in a giant try/catch
+         * put the try/catch on client-end, but cancelling isn't an error, so display operation summary with error summary?
+         */
+        if (!alert('Abort the operation?', true))
+            return true;
+        throw Error('User aborted the operation');
+        return false;
+    };
+    
+    (this.w = w) && (w = null);
+};
+
+ProgressWindow.prototype.init = function () {
+    this.start = new Date();
+    this.w.show();
+};
+
+ProgressWindow.prototype.update = function (name) {
+    if (name) {
+        this.file = name;
+        this.bar.value = ++this.count;
+    }
+    this.timer = (new Date() - this.start) / 1000;
+    this.tpass = formatTime(this.timer);
+    this.tleft = formatTime((--this.total) * (this.timer / this.count));
+    this.formatText(this);
+    // use idletask to "pump" event loop aka window.update()?
+    // determine whether it is necessary to call update, and how enableRedraw affects things
+};
+
+ProgressWindow.prototype.formatText = function (obj) {
+    for (var txt in obj.texts) {
+        txt.text = txt.pattern.replace(/\{(\w+)\}/g, function (match, g1) {
+            return obj.hasOwnProperty(g1) ? obj[g1] : '';
+        });
+    }
+};
+
+ProgressWindow.prototype.close = function () {
+    this.w.onClose = null;
+    this.w.close();
+    if (!this.total)
+        alert(localize('Operation complete!\nProcessed %1 INDD files in %2',
+                       this.count, formatTime((new Date() - this.start) / 1000));
+};
+
+function formatTime (time) {
+    var u, t = [];
+    if (u = Math.floor(time / 3600))
+        t.push(u + (u > 1 ? ' hours' : ' hour'));
+    if (u = Math.floor(time % 3600 / 60))
+        t.push(u + (u > 1 ? ' minutes' : ' minute'));
+    if (time < 3600 && (u = Math.floor(time % 60)))
+        t.push(u + (u > 1 ? ' seconds' : ' second'));
+    return t.join(', ');
+}
+
+function buildBar (parent) {
+    var bar = parent.add('customBoundedValue {text: "0%", factor: 0}');
+    bar.w = bar.graphics.measureString('X')[0] * 60;
+    bar.h = bar.graphics.measureString('X')[1] * 2;
+    bar.back: bar.graphics.newBrush(+null, [0.4, 0.4, 0.4]),
+    bar.fill: bar.graphics.newBrush(+null, [0.13, 0.59, 0.95])
+    bar.pen = bar.graphics.newPen(+null, [1, 1, 1], 1);
+    bar.onChange = function () {
+        this.factor = this.value / this.maxvalue;
+        this.text = (this.factor * 100).toFixed(2) + '%';
+        this.notify('onDraw');
+    };
+    bar.onDraw = function () {
+        this.graphics.rectPath(0, 0, this.w, this.h),
+        this.graphics.fillPath(this.back);
+        this.graphics.closePath();
+        this.graphics.newPath();
+        this.graphics.rectPath(0, 0, (this.factor) * this.w, this.h);
+        this.graphics.fillPath(this.fill);
+        this.graphics.drawString({
+            text: this.text,
+            pen: this.pen,
+            x: (this.w - this.graphics.measureString(this.text)[0]) / 2,
+            y: this.h / 4
+        });
+    };
+    return bar;
+}
