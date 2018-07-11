65e28fe0aacaf4890c18f27e455b15353efa67d5
Erik Morgan <morgan.erik@outlook.com>
2018-07-10 21:31:37 -0500

    trackUsage refactored, and all done except logging

--- Python/trackUsage.py
+++ Python/trackUsage.py
@@ -3,57 +3,53 @@ from pathlib import Path
 from zipfile import ZipFile
 import re
 from lxml import etree
+from datetime import datetime
 
-# Parse outlines, extract running procedures, and list them with the SM #
-# If line begins has 5 numbers in a row, or begins with 2-, 4-, or 6-, skip it
-# If line has less than 3 numbers, skip it
-# Ignore leading/trailing whitespace
-# 
-# ABER PROC|AP|BZ|CC|CFS|CR|CRC|CRJ|CRM|CSRP|CWS|DR|DRC|DRM|DTAP|DTCC|DTDR|DTMC|DTMS|DTSC|DTSS|DTUW|DV|ER|FD|FDC|FDM|GVS|GPU|HPU|LS|MC|MDTCC|MS|MSCM|PMP|POS|RT|SC|SCC|SCCM|SCJ|SCMS|SING|SS|SSC|SSH|SSJ|SW|TUTU|UTA|UW|UWHTSM?|WOC
-# 
-# OPTIMIZED:
-# \b(ABER.?PROC|BZ|CFS|CR[CJM]?|CSRP|CWS|(DT)?(AP|[CMS]{2,4}|DR|UW)|DV|ER|F?DR?[CM]?|GVS|GPU|HPU|LS|MDTCC|PMP|POS|RT|SCJ|SING|SSH|SSJ|SW|TUTU|UTA|UWHTSM?|WOC)\b
+# decided to record everything instead of just latest so i can have all data for other potential uses
 
 root = Path('/Users/HD6904/Erik/Procedure Status/outlines')
 log = Path('/Users/HD6904/Erik/Procedure Status/parser-results.txt')
-rpre = r'\b(ABER.?PROC|AP|BZ|CFS|[CMS]{2,4}|CR[CJM]?|CSRP|CWS|DR[CM]?|'
-       + r'DT(AP|[CMS]{2}|DR|UW)|DV|ER|FD[CM]?|GVS|GPU|HPU|LS|MDTCC|PMP|POS|'
-       + r'RT|SCJ|SING|SSH|SSJ|SW|TUTU|UTA|UW(HTSM?)?|WOC)\s*\d{2,4}'
-procs = {}
+procre = (r'\b(?:ABER.?PROC|AP|BZ|CC|CFS|CR|CRC|CRJ|CRM|CSRP|CWS|DR|DRC|DRM|'
+           'DTAP|DTCC|DTDR|DTMC|DTMS|DTSC|DTSS|DTUW|DV|ER|FD|FDC|FDM|GVS|GPU|'
+           'HPU|LS|MC|MDTCC|MS|MSCM|PMP|POS|RT|SC|SCC|SCCM|SCJ|SCMS|SING|SS|'
+          r'SSC|SSH|SSJ|SW|TUTU|UTA|UW|UWHTSM?|WOC)\s*\d{2,4}(?:\S\w+)*')
+jobs = {}
+# use tuples like (rp, sm, date)
 
 for f in root.iterdir():
+    manualNumber = re.search(r'\d{4}', f.name)[0]
     with ZipFile(f) as zip:
         xdoc = zip.open('word/document.xml').read().decode()
     xdoc = re.sub(r'\b(r|w[a-z0-9]*):(?! )| xmlns[^>]+| encoding=\S+', '', xdoc)
-    props = {
-        'sm': re.search(r'\d{4}', f.name)[0],
-        'rps': []
-    }
-    doc = etree.fromstring()
-    for para in getText(doc):
-        if 'due' not in props and re.match(r'(DRAFT )?DUE', para, re.I):
-            props['due'] = re.sub(r'.*DUE( DATE)?[: ]+', '', para.upper())
-        else:
-            # this is where i test for procedures
-    props['rpids'] = getProcs(text)
-    procs[f.name] = props
-
-def getProcs(text):
-    match = re.search(r'\b(ABER.?PROC|BZ|CFS|CR[CJM]?|CSRP|CWS|(DT)?'
-                      r'(AP|[CMS]{2,4}|DR|UW)|DV|ER|F?DR?[CM]?|GVS|GPU|HPU|'
-                      r'LS|MDTCC|PMP|POS|RT|SCJ|SING|SSH|SSJ|SW|TUTU|UTA|UWHTSM?|WOC)\b', text)
+    doc = etree.fromstring(re.sub(r'<tab/>', '<t>\t</t>', xdoc))
+    text = getText(doc)
+    dueDate = formatDate(
+        re.search(r'DUE.+?(\d\d)[-/ ]?(\w+)[-/ ]?(\d\d)').expand(r'\1\2\3')
+    )
+    jobs[f.name[:-5]] = (re.findall(procre, text), manualNumber, dueDate)
 
 def getText(doc):
+    text = []
+    app = text.append
     for p in doc.iter('p'):
-        nodes = [e for r in p.iter('r') for e in r.iter('t', 'tab', 'br')]
-        paraText = ''
-        for node in nodes:
-            tag = node.tag
-            if tag == 'br':
-                break
-            if tag == 't':
-                paraText += node.text
-            elif tag == 'tab':
-                paraText = paraText.strip() + '\t'
-        if paraText.strip() and not re.search(r'^\d-|[-0-9]\d{5}', paraText):
-            yield paraText
+        paraText = ''.join(list(p.itertext())).strip()
+        if paraText and not re.match(r'\s*(\d-|[A-Z]+ ?)\d{5}', paraText, re.I):
+            app(paraText)
+    return '\n'.join(text).upper()
+
+def formatDate(dateString):
+    dateString = dateString.replace('-', '/')
+    if '/' in dateString:
+        d = datetime.strptime(dateString, '%m/%d/%y')
+    else:
+        d = datetime.strptime(dateString, '%m-%d-%y')
+    return str(d.date())
+
+def logProps(propObj):
+    # rethink the procs/props structure...problem is not being able to decipher/rely
+    # on due date formatting...unless I change them ahead of time
+    # PROBLEM: using all procs, then manipulating data results in 100s of the same SS-15 file
+    with open(log, 'a') as f:
+        for job in jobs:
+            # write (proc, manualNumber, dueDate) to each line
+            f.write()
