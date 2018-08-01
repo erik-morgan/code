f5f1c31940b14e26183f3c1e4053735e1195488a
Erik Morgan <morgan.erik@outlook.com>
2018-07-31 23:12:27 -0500

    added logger to batch, tested include, and refactored helper structure

--- JavaScript/InDesign/batch.jsx
+++ JavaScript/InDesign/batch.jsx
@@ -4,49 +4,22 @@
 (function (global) {
     Folder.current = File($.fileName).parent;
     
-    var hasOwn = Object.prototype.hasOwnProperty,
-        decode = global.decodeURI;
-    
-    // load deps in here, so they're guaranteed to exist when processing
-    // evalFile may be totally unnecessary if the following line works
+    var decode = global.decodeURI;
+
     #include 'helpers.jsx';
-    // if this works, put hasOwn, decode, and except into helpers
     
-    function evalFile (f) {
-        if (!(f = File(f)).exists)
-            except('nodep', f, 'evalFile');
-        f.open() && eval(f.read());
-        f.close();
-    }
-    
-    function except (type, arg, func) {
-        var e = type instanceof Error ? type : 
-                new Error(
-                    type == 'nodep' ? 'Missing required dependency: ' + arg :
-                    type == 'process' ? 'No processes in Batch.processes' :
-                    type == 'queue' ? 'No documents in Batch.queue' : arg
-                );
-        if (func) {
-            e.from = func instanceof Function ? func.name : func;
-        } else if (type == 'process' || type == 'queue') {
-            e.from = 'Batch.process';
-        }
-        if (Batch && Batch.logging) {
-            Batch.log(e);
-        }
-        throw e;
-    }
-    
-    function doc (file) {
+    function newDoc (file) {
         if (!file.exists) {
-            Batch.log('error.file', file);
+            Batch.log('dne', file);
         }
         var name = decode(file.name.toUpperCase());
-        this.file = file;
-        this.prefix = /[A-Z]+/.exec(name)[0];
-        this.id = name.match(/^\S+?(?=\.)/)[0];
-        this.rev = /\bR\d{1,2}\b/.test(name) ? 
-                   name.match(/\bR(\d{1,2})\b/)[0] : 0;
+        return {
+            file: file;
+            prefix: /[A-Z]+/.exec(name)[0];
+            id: name.match(/^\S+?(?=[. ])/)[0];
+            rev: /\bR\d{1,2}\b/.test(name) ? 
+                 name.match(/\bR(\d{1,2})\b/)[0] : 0;
+        }
     }
     
     function Batch () {
@@ -55,22 +28,15 @@
         }
         this.processes = [];
         this.queue = [];
-        this.logging = true;
     }
     
-    Batch.prototype.process = function () {
+    Batch.prototype.process = function (doc) {
         // still have to add ui & indesign events
-        (this.processes.length || except('process')) && (this.queue.length || except('queue'));
         if (this.queue.length == 0) {
             except('queue');
-        }
-        if (this.processes.length == 0) {
+        } else if (this.processes.length == 0) {
             except('process');
         }
-    };
-    
-    Batch.prototype.log = function (scope, data) {
-        
     };
     
     global.Batch = Batch();
--- JavaScript/InDesign/helpers.jsx
+++ JavaScript/InDesign/helpers.jsx
@@ -1,26 +1,29 @@
-﻿var keys = function (obj) {
+﻿var hasProp = function (obj, prop) {
+    return Object.prototype.hasOwnProperty.call(obj, prop) &&
+           !obj[prop] instanceof Function;
+};
+var keys = function (obj) {
     return items.call(obj, true);
-}
+};
 var values = function (obj) {
     return items.call(obj, false);
-}
+};
 var items = function (obj, k) {
     if (!obj || obj !== Object(obj)) {
         throw TypeError('Can not get keys of a non-object');
     }
     var a = [],
-        hasOwn = Object.prototype.hasOwnProperty,
         isEnum = Object.prototype.propertyIsEnumerable;
     for (var p in obj) {
-        if (hasOwn.call(obj, p)) {
+        if (hasProp(obj, p) && isEnum.call(obj, p)) {
             a.push(k ? p : k == undefined ? [p, obj[p]] : obj[p]);
         }
     }
     return a;
-}
+};
 var trim = function (str) {
     return str.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '').replace(/ {2,}/g, ' ');
-}
+};
 var includes = function (arr, member, fromIndex) {
     if (arr === null || arr === undefined)
         throw TypeError('arr is null or undefined');
@@ -34,19 +37,7 @@ var includes = function (arr, member, fromIndex) {
         if (i in o && (o[i] === member || val(o[i]) === val(member)))
             return true;
     }
-}
-var merge = function (arr, args) {
-    if (arr === null || arr === undefined)
-        throw TypeError('arr is null or undefined');
-    if (!arr instanceof Array)
-        throw TypeError('Can not merge into non-Array');
-    var i = arr.length,
-        len = arguments.length;
-    for (var n = 0; n < len; n++) {
-        arr[i++] = arguments[n];
-    }
-    arr.length = i;
-}
+};
 var each = function (arr, callback) {
     if (arr === null || arr === undefined)
         throw TypeError('arr is null or undefined');
@@ -56,7 +47,7 @@ var each = function (arr, callback) {
     for (var i = 0; i < arr.length; i++) {
         callback.call(thisVal, arr[i], i);
     }
-}
+};
 var map = function (arr, callback) {
     if (arr === null || arr === undefined)
         throw TypeError('arr is null or undefined');
@@ -68,4 +59,81 @@ var map = function (arr, callback) {
         a[i] = callback.call(thisVal, arr[i], i);
     }
     return a;
+};
+
+(function () {
+    // consider opening file here, and writing throughout?
+    var logger = {};
+    logger.levels = {NONE: 0, ERROR: 1, INFO: 2, DEBUG: 4};
+    logger.level = this.levels.NONE;
+    logger.file = Folder.current + '/batch.log';
+    logger.logs = [];
+    logger.log = function (level, data) {
+        this.logs.push(localize(' [%1-%2-%3T%4] | %5 | %6'));
+        // integrate timestamp here
+        msg = timestamp() + '  [' + level + ']  ' + toString(data);
+    };
+    logger.error = function (data) {
+        if ((this.level * 2 - 1) & this.levels.ERROR)
+            this.log('ERROR', data);
+    };
+    logger.info = function (data) {
+        if ((this.level * 2 - 1) & this.levels.INFO)
+            this.log('INFO ', data);
+    };
+    logger.debug = function (data) {
+        if ((this.level * 2 - 1) & this.levels.DEBUG)
+            this.log('DEBUG', data);
+    };
+    this.levels.ERROR | this.levels.INFO | this.levels.DEBUG
+    return logger;
+})();
+
+logger.log = function (level, data) {
+    var d = new Date();
+    this.logs.push(localize(' [%1-%2-%3T%4] | %5 | %6', 
+                            d.getFullYear(),
+                            ('0' + (++d.getMonth())).substr(-2),
+                            ('0' + d.getDate()).substr(-2),
+                            d.toTimeString().slice(0, 8),
+                            level, toString(data, pretty));
+    // toString options have to start with 31 spaces to match logs
+    // cap log width at 120
+    // just do json/toString for objects/arrays
+};
+
+logger.str = function (obj) {
+    if (obj === o) {
+        var o = Object(obj);
+        var elements = map(o.items(), function (element) {
+            if (obj instanceof Array) {
+                return logger.str(element[0]);
+            } else {
+                return element[0] + ': ' + logger.str(element[1]);
+            }
+        })
+        // figure out how to handle object vs array
+        // reference fast-json-stable-stringify on github
+        var brackets = o.toString() === '[object Array]' ? '[]' : '{}';
+    } else {
+        return String(obj);
+    }
+};
+
+function except (type, arg, func) {
+    var e = type instanceof Error ? type : 
+            new Error(
+                type == 'process' ? 'No processes in Batch.processes' :
+                type == 'queue' ? 'No documents in Batch.queue' : arg
+            );
+    if (func) {
+        e.from = func instanceof Function ? func.name : func;
+    } else if (type == 'process' || type == 'queue') {
+        e.from = 'Batch.process';
+    }
+    if (Batch && Batch.logging) {
+        Batch.log('err', e);
+    }
+    throw e;
 }
+
