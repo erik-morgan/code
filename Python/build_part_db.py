6e932d870c18711a29ca19b6a3ed137490805171
Erik Morgan <morgan.erik@outlook.com>
2018-08-16 21:08:39 -0500

    build_part_db update; todo: sqlite db part & testing

--- Python/build_part_db.py
+++ Python/build_part_db.py
@@ -1,77 +1,67 @@
-#!/usr/bin/python
 from pathlib import Path
-from os import listdir
-from zipfile import ZipFile
+from datetime import datetime
 import re
-from lxml import etree
+import sqlite3
 
 root = Path('/Users/HD6904/Erik/Procedure Status/outlines')
-rp_dwg_re = (r'\b(?:C[FW]S|[CD]R[CJM]?|CSRP|DV|ER|FD[CM]?|GVS|[GH]PU|'
-             '(?:DT)?(?:AP|[CMS]{2,4}|DR|UW)|LS|PMP|POS|RT|S[CS]J|SING|SSH|SW|'
-             r'UWHTSM?|WOC) ?\d{3,4}\S*|^(?:\d-)?\d{6}[-\d]+')
-date_re = r'DUE.+?(\d\d)[-/ ]?(\w+)[-/ ]?(\d\d)'
-
-# ALLOW FOR #-###### drawings (no dash number)
+db_file = Path('/Users/HD6904/Erik/Procedure Status/proc_parts.db')
+raw_file = Path('/Users/HD6904/Erik/Procedure Status/proc_parts_raw.txt')
+# rp_dwg_re = (r'^(?:C[FW]S|[CD]R[CJM]?|CSRP|DV|ER|FD[CM]?|GVS|[GH]PU|'
+#              '(?:DT)?(?:AP|[CMS]{2,4}|DR|UW)|LS|PMP|POS|RT|S[CS]J|SING|SSH|SW|'
+#              r'UWHTSM?|WOC) ?\d{4}\S*|^(?:\d-)?\d{6}\S*')
+# rp_re = re.compile(r'^[A-Z]{2,6}\d{4}.*', re.I)
+# rpdw_re = re.compile(r'[A-Z]{2,6}\d{4}.*|(?:\d-)?\d{6}.*')
+rpdw_re = re.compile(r'^([A-Z]{2,6}\d{4}\S*).*\n?(?:\n\S*\d{6})+', re.I|re.M)
+draw_re = re.compile(r'^(?:\d-)?\d{6}\S*', re.M)
+date_re = re.compile(r'DUE.+?(\d\d)[-/ ]?(\w+)[-/ ]?(\d\d)')
 
 def main():
+    # REMOVE SUDS FROM OUTLINES
+    # REFORMAT PROCS WHEN ADDING TO DB
     procs = {}
-    docxs = [f for f in listdir(root) if not f[0] != '.']
-    progress = 'Processing File {0}/' + len(docxs) + ' ({1})'
-    for i, docx in enumerate(docxs):
-        doc = root / docx
-        print(progress.format(i + 1, docx))
-        doc_txt = parse_doc(doc).upper()
-        due_date = formatDate(re.search(date_re, text).expand(r'\1\2\3'))
-        # cleanup proc numbers separately, and delete if "based on"
-        proc, draws = '', []
-        for match in re.finditer(rp_dwg_re, doc_txt):
+    raw_data = []
+    raw_app = raw_data.append
+    docs = [txt.name for txt in root.glob('*.txt')]
+    prog_temp = 'Processing File {0}/' + len(docs) + ' ({1})'
+    for i, name in enumerate(docs):
+        print(progress.format(i + 1, name))
+        doc = root / name
+        doc_num = re.search(r'\d{4}', name)[0]
+        doc_txt = doc.read_text().upper()
+        doc_date = formatDate(date_re.search(doc_txt).expand(r'\1\2\3'))
+        for match in re.finditer(rpdw_re, doc_txt):
             match = match[0]
-            if match[0].isdigit() and proc:
-                draws.append(match)
-            else:
-                if draws:
-                    draws = tuple(draws)
-                    if draws not in procs[proc] or due_date > procs[proc][draws]:
-                        procs[proc][draws] = due_date
-                proc = format_proc(match)
-                draws = []
-                if not proc in procs:
-                    procs[proc] = {}
-        return procs
+            draws = draw_re.findall(match)
+            if draws:
+                proc = re.sub(r'\W', '-', match[1])
+                draws = ', '.join(sorted(draws))
+                raw_app('\t'.join(doc_num, proc, str(doc_date), draws))
+                rpdw = (proc, draws)
+                if not rpdw in procs or procs[rpdw][0] < doc_date:
+                    procs[rpdw] = (doc_date, doc_num)
+    raw_file.write_text('\n'.join(raw_data))
+    dump_data(procs)
 
-def parse_doc(doc_file):
-    with ZipFile(doc_file) as zip:
-        xdoc = zip.open('word/document.xml').read().decode()
-    xdoc = re.sub(r'\b(aink|am3d|cx\d?|mc?|[orv]|w\w*):(?! )| xmlns[^>]+| encoding=\S+', '', xdoc)
-    doc = etree.fromstring(xdoc.replace('<tab/>', '<t>\t</t>'))
-    return '\n'.join(p for p in get_text(doc))
-
-def get_text(doc):
-    for p in doc.iter('p'):
-        para_text = ''.join(list(p.itertext())).strip()
-        if para_text and re.search(r'[0-9]{3}', para_text):
-            yield para_text
-
-def format_proc(proc):
-    # full match: ([A-Z]+)\s*(\d{2,4})(.?\d\d)?(.?\d\d)?(.?\d\d)?(.?[A-Z]+)?
-    base = re.match(r'([A-Z]+)\s*(\d{2,4})(?=\W)', proc)
-    if base:
-        proc = proc.replace(base[0], base[1] + base[2].rjust(4, '0'))
-    ends = re.split(r'[^A-Z]+', proc)
-    digits = ''.join(c for c in proc if c.isdigit())
-    if len(digits) % 2 != 0:
-        digits = '0' + digits
-    return re.sub(r'(?<=\d\d)(\d\d)(?=\d\d)', r'\1-', digits).join(ends)
+def dump_data(procs):
+    conn = sqlite3.connect(db_file)
+    c = conn.cursor()
+    # Create table
+    c.execute('''CREATE TABLE stocks
+              (date text, trans text, symbol text, qty real, price real)''')
+    # Insert a row of data
+    c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+    # Save (commit) the changes
+    conn.commit()
+    # We can also close the connection if we are done with it.
+    # Just be sure any changes have been committed or they will be lost.
+    conn.close()
 
 def format_date(date_str):
     if date_str.isnumeric():
         d = datetime.strptime(date_str, '%m%d%y')
     else:
         d = datetime.strptime(date_str, '%d%b%y')
-    return str(d.date())
-
-def parse_data(data):
-    pass
+    return d.date()
 
 if __name__ == '__main__':
     log = Path('/Users/HD6904/Erik/Procedure Status/part_data.txt')
