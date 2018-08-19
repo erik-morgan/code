6f403d6fcfb669952cf766f253c63f7c241535ea
Erik Morgan <morgan.erik@outlook.com>
2018-08-18 22:06:26 -0500

    finished build_modules & needs testing

--- Python/build_modules.py
+++ Python/build_modules.py
@@ -1,66 +1,86 @@
-# 2018-08-18 00:00:09 #
-from os import walk, chdir, getcwd, sep
-from os.path import exists, isdir, dirname, basename
-from fnmatch import fnmatch, filter as fnfilter
-from PyPDF2 import PdfFileReader as Reader, PdfFileMerger as Merger, PdfFileWriter as Writer
+# 2018-08-18 22:01:51 #
 import sys
+from os import walk, getcwd, remove
+from os.path import exists, isdir
+from fnmatch import filter as fnfilter
+from PyPDF2 import PdfFileReader as Reader, PdfFileMerger as Merger
 import re
 
 # TODO: move dwgs from Libraries to Drawings
 # TODO: finally standardize proc naming to SS####.R#.ext
+# NOTE: could benefit from a coroutine setup where the builder appends to Merger
+#       while none of the files are missing. then, if missing, it'll keep a list of
+#       missing files. then, at end, when next is triggered without a send, it'll
+#       evaluate missing list, and either write to file (will need to somehow accept path)
+#       or print the missing message
 
+PDFS_PATH = '/Users/HD6904/Desktop/PDFs'
+DRAWS_PATH = '/Users/HD6904/Desktop/Drawings'
 drawre = re.compile(r'^ *[-0-9A-Z]+ ?\d{5}\S*', re.I|re.M)
-missing_msg = 'Skipping "{0}"...Could not find file(s): {1}'
 
-def main(dirs):
-    for dir_path in dirs:
-        tocs = [f for f in parse_dir(dir_path) if fnmatch(f, '*TOC.*')]
-        for toc in tocs:
-            files, missing = parse_toc(toc)
-            if missing:
-                print(missing_msg.format(basename(toc), ', '.join(missing)))
+def main(proj):
+    proj_tocs = proj + '/TOCs/'
+    proj_pdfs = proj + '/PDFs/'
+    if exists(proj_tocs) and exists(proj_pdfs):
+        for toc_path in iter_dir(proj_tocs, '*TOC.*'):
+            name, proc = re.search(r'.+/(([^. ]+).+)', toc_path).groups()
+            merge_list = [toc_path, get_path(proc)]
+            for draw in scrape_toc(toc_path):
+                merge_list.append(get_path(draw))
+            if all([exists(f) for f in merge_list]):
+                build_module(merge_list, proj_pdfs + proc + '.pdf')
             else:
-                build_module(files, toc)
+                print(f'{name} failed to build due to missing files: ' +
+                      ', '.join(f for f in merge_list if '/' not in f))
+    else:
+        print('The project directory must contain "TOCs" and "PDFs" folders.\n'
+              'If called without an argumemt, working directory is used.')
 
-def parse_dir(dir_path):
-    if exists(dir_path) and isdir(dir_path):
-        for path, dirs, files in walk(dir_path):
-            path = path + sep
-            for f in files:
-                if f[-3:].lower() == 'pdf':
-                    yield path + f
-
-def parse_toc(toc):
-    missing = []
-    cat_list = [toc, get_path(basename(toc), True)]
-    if not cat_list[1]:
-        missing.append('Procedure PDF')
+def scrape_toc(toc):
     with open(toc, 'rb') as f:
         txt = '\n'.join(pg.extractText() for pg in Reader(f).pages)
-        for draw in drawre.finditer(txt):
-            draw_path = get_path(draw)
-            if draw_path:
-                cat_list.append(draw_path)
-            else:
-                missing.append(draw)
-    return cat_list, missing
+    for draw in drawre.finditer(txt):
+        yield draw
 
-def get_path(file_id, is_proc=False):
-    pattern = '*/' + file_id + '*'
-    path = fnfilter(PDFS if is_proc else DRAWS, pattern)
-    if not path and not is_proc:
-        return get_path(file_id.rsplit('-', 1)[0] + '.')
-    return path[0] if path else None
+def build_module(path_list, out_path):
+    if exists(out_path):
+        os.remove(out_path)
+    pdf = Merger()
+    for path in path_list:
+        pdf.append(path)
+    pdf.write(out_path)
 
-def build_module(path_list, toc):
-    # use Merger class, and write to module_path
-    toc = re.sub(r'(.+)TOCs(.+).TOC', r'\1PDFs\2', toc)
-    # LEFT OFF HERE
+def iter_dir(dir_path, cond='*'):
+    for path, dirs, files in walk(dir_path):
+        path += '/'
+        files = [path + f for f in fnfilter(files, '*.[Pp][Dd][Ff]')]
+        for f in fnfilter(files, cond):
+            yield f
+
+def get_path(idnum):
+    # draw test without using re:
+    # if len(max(''.join(str(int(c.isdigit())) for c in idnum).split('0'))) > 4:
+    pattern = '*/{0}*'.format
+    if re.search(r'[0-9]{5}', idnum):
+        paths = fnfilter(draws, pattern(idnum + '.'))
+        if not paths:
+            paths = fnfilter(draws, pattern(idnum.rsplit('-', 1)[0] + '.'))
+    else:
+        paths = fnfilter(pdfs, pattern.format(idnum))
+    return paths[0] if paths else idnum
+
+def test_dir(path):
+    if exists(path) and isdir(path):
+        return True
+    print(f'{path} is not a valid directory.')
+    return False
 
 if __name__ == '__main__':
-    PDFS = list(parse_dir('/Users/HD6904/Desktop/PDFs'))
-    DRAWS = list(parse_dir('/Users/HD6904/Desktop/Drawings'))
-    if len(sys.argv) == 1:
-        main(getcwd())
-    else:
-        main(sys.argv[1:])
+    proj_path = sys.argv[1] if len(sys.argv) > 1 else getcwd()
+    if test_dir(PDFS_PATH) and test_dir(DRAWS_PATH):
+        pdfs = list(iter_dir(PDFS_PATH))
+        draws = list(iter_dir(DRAWS_PATH))
+        if len(sys.argv) == 1:
+            main(getcwd())
+        else:
+            main(sys.argv[1])
